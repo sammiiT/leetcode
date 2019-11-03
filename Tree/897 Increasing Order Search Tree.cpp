99a3b4ae8cf00e8d66024c8727c719e2e4bd0146
sammiiT <dirsing@gmail.com>
2019-11-03 11:09:15 +0800

    1.0

---
+++ Tree/897 Increasing Order Search Tree.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode *res=NULL, *last=NULL;
+        
+        helper(root,&last,&res);
+        return res;
+    }
+    
+    //recursive往上傳, last, res是副本,所以用pointer會出錯, 必須用pointer to pointer
+    void helper(TreeNode* root, TreeNode** last, TreeNode** res){
+        if(!root) return;
+        
+        helper(root->left,last,res);
+        if(*res==NULL){
+            *res=new TreeNode(root->val);
+            *last = *res;
+        }else{
+            (*last)->right = new TreeNode(root->val);
+            (*last) = (*last)->right;
+        }
+        helper(root->right,last,res);
+        
+    }
+    
+};
