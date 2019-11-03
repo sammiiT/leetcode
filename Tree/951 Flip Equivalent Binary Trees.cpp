c027475b3cac205940ea47fd23277b92fa83c89a
sammiiT <dirsing@gmail.com>
2019-11-03 10:41:32 +0800

    1.0

---
+++ Tree/951 Flip Equivalent Binary Trees.cpp
@@ -0,0 +1,14 @@
+﻿class Solution{
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(!root1 && !root2) return true;
+        
+        if(!root1&&root2 || root1&&!root2 || root1->val!= root2->val) return false;
+        
+        return (flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left))
+            ||(flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right));
+
+        //中間可以不對調,或可對調, 所以要加上(root1->left,root2->left),(root1->right,root2->right)
+        // logic operator運算順序 && , ||
+    }
+};
