52f741fb9da2d60a081efeb3fa6e96cdac967c81
sammiiT <dirsing@gmail.com>
2022-10-30 00:55:03 +0800

    Update 3Sum.cpp

--- array/3Sum.cpp
+++ array/3Sum.cpp
@@ -6,12 +6,13 @@ public:
         sort(nums.begin(),nums.end());
         
         for(int i=0; i<n-2; ++i){
-            if(i>0 && nums[i]==nums[i-1]) continue;    
+            
+            if(i>0 && nums[i]==nums[i-1]) continue; //避免i重複選取   
             // if(i==0 || nums[i]!=nums[i-1])//OK statement
-            for(int j=i+1,k=n-1;j<k;){
-                if(j-1>i && nums[j-1]==nums[j]){//j??:???(j)=???(j-1)
+            for(int j=i+1,k=n-1;j<k;){//在一輪執行完畢之後,可以找出最多n組解, 所以i再增加時,如果數值跟前一個一樣, 會造成重複解.
+                if(j-1>i && nums[j-1]==nums[j]){//避免j重複選取
                     ++j;
-                }else if(k+1<n && nums[k]==nums[k+1]){//k??:???(k)=???(k+1)
+                }else if(k+1<n && nums[k]==nums[k+1]){//避免k重複選取
                     --k;
                 }else{
                     int sum = nums[i]+nums[j]+nums[k];
