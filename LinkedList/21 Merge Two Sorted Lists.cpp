87cdb783c5dd957def92fe1077e2a9815115246e
sammiiT <dirsing@gmail.com>
2022-10-29 22:53:50 +0800

    Update 21 Merge Two Sorted Lists.cpp

--- LinkedList/21 Merge Two Sorted Lists.cpp
+++ LinkedList/21 Merge Two Sorted Lists.cpp
@@ -19,7 +19,31 @@ public:
             cur->next = new ListNode(input);
             cur=cur->next;
         }
-        
         return dummy->next;
     }
+
+    ListNode* mergeTwoLists_2(ListNode* list1, ListNode* list2) {
+        ListNode *cur = NULL;
+        ListNode hdr(-1);
+        cur = &hdr; //如果用當下新建的節點, 則沒辦法知道上一個節點
+                    //所以新建節點必須用cur->next來指派
+        while(list1||list2){
+            int val1 = list1? list1->val : INT_MAX;
+            int val2 = list2? list2->val : INT_MAX;
+            //int merge = 0;
+                
+            if(val1 <= val2){
+                cur->next = new ListNode(val1);
+            //    cur = cur->next;
+                list1 = list1 ? list1->next:NULL;    
+            } else if(val1 > val2){
+                cur->next = new ListNode(val2);
+             //   cur = cur->next;
+                list2 = list2 ? list2->next: NULL;
+            } 
+            cur = cur->next;
+        }
+        return hdr.next;
+    }	
+	
 };
