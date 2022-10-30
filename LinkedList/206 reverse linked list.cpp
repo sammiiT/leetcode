24c5115e94a59106838ccf79808ecd4b20a849fd
sammiiT <dirsing@gmail.com>
2022-10-30 11:02:19 +0800

    Update 206 reverse linked list.cpp

--- LinkedList/206 reverse linked list.cpp
+++ LinkedList/206 reverse linked list.cpp
@@ -38,4 +38,24 @@ public:
         return pre;
         
     }
+    
+     ListNode* reverseList_2(ListNode* head) {
+        ListNode *prev,*cur,*tmp;
+        
+        if(!(head&&head->next)) return head;
+        
+        prev = NULL;
+        cur = head;
+        tmp = cur->next;
+        
+        while(cur){
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+            tmp = tmp?tmp->next:NULL;
+        }
+        return prev;
+        
+    }
+    
 };
