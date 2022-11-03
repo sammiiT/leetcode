399cde4b41b5524c0c97be30f71256831a96666a
sammiiT <dirsing@gmail.com>
2022-11-03 14:37:12 +0800

    Update 725 Split Linked List in Parts.cpp

--- LinkedList/725 Split Linked List in Parts.cpp
+++ LinkedList/725 Split Linked List in Parts.cpp
@@ -57,7 +57,66 @@ public:
         return res;
     }
 };
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ret;
+        int n = 0;
+    
+        for(ListNode* t = head; t; t=t->next) n++;
+    
+        if(n/k){//每一個partition有(n/k) node
+            if(n%k){//沒有整除
+                ListNode *start,*move,*nstart;
+                start = move = nstart = head;
+                int remain = 1;//整除下來剩下的node, 要平均分配在不同的list
+                int j = 1;//平均分配, 每一次分一個
 
+                while(start){
+                    for(int i=0; i<(n/k)-1+j; i++){
+                        move = move->next;
+                    }
+                    nstart = move->next;
+                    move->next = NULL;
+                    ret.push_back(start);
+                    start = nstart;
+                    move = nstart;
+                    remain++;
+                    if(remain>(n%k)){
+                        j = 0;
+                    }
+                }
+            }else{//整除
+                ListNode *start,*move,*nstart;
+                start = move = nstart = head;
+
+                while(start){
+                    for(int i=0; i<(n/k)-1; i++){
+                        move = move->next;
+                    }
+                    nstart = move->next;
+                    move->next = NULL;
+                    ret.push_back(start);
+                    start = nstart;
+                    move = nstart;
+                }
+            }
+
+        } else {
+            ListNode *t;
+            ListNode *cur = head;
+            for(int i =0; i<k; i++){
+                if(cur){
+                    t = cur->next;
+                    cur->next = NULL;
+                    ret.push_back(cur);
+                    cur = t;
+                }else{//cur = NULL;
+                    ret.push_back(NULL);
+                }
+            }
+        }
+
+        return ret;        
+    }
 
 // for(int i=0;i<k;++i){
 //                 if(remain){
