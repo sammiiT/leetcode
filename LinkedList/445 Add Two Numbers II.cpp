58ec94546a679da1e9740ba09e4d11b2355e648c
sammiiT <dirsing@gmail.com>
2022-11-03 14:35:52 +0800

    Update 445 Add Two Numbers II.cpp

--- LinkedList/445 Add Two Numbers II.cpp
+++ LinkedList/445 Add Two Numbers II.cpp
@@ -79,8 +79,48 @@ public:
             sum = sum/10;
         }
         return res->val==0 ? res->next:res; 
-        
     }
+    
+ListNode* reverse_helper(ListNode* head){
+        ListNode *prev,*cur;
+        prev = NULL;
+        cur = head;
+        for(;cur!=NULL;){
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+}
+ListNode* addTwoNumbers_2(ListNode* l1, ListNode* l2) {
+        ListNode *l_1,*l_2,*ret;
+        ListNode vnode(-1);
+        ListNode *cur;
+        int carry = 0;
+        int sum = 0;
+        l_1 =reverse_helper(l1);
+        l_2 = reverse_helper(l2);
+        cur = &vnode;
+        while(l_1||l_2){
+            int val1=l_1?l_1->val:0;
+            int val2=l_2?l_2->val:0;
+            
+            sum = val1 + val2 + carry;
+            cur->next = new ListNode(sum%10);
+            cur = cur->next;
+            carry = sum/10;
+            
+            if(l_1) l_1 = l_1->next;
+            if(l_2) l_2 = l_2->next;
+        }
+        if(carry){
+            cur->next = new ListNode(carry);
+        }
+        ret = reverse_helper(vnode.next);
+        return ret;
+}   
+    
 };
 
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
