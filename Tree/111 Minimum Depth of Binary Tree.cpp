d5c18ae24b0ec3c90c9763cd36c842b37a451f1c
sammiiT <dirsing@gmail.com>
2022-11-09 22:14:10 +0800

    Update 111 Minimum Depth of Binary Tree.cpp

--- Tree/111 Minimum Depth of Binary Tree.cpp
+++ Tree/111 Minimum Depth of Binary Tree.cpp
@@ -11,12 +11,19 @@ public:
         return 1+ res;
     }
 
-	int minDepth_OK(TreeNode* root){
-		if (!root) return 0;
+int minDepth_OK(TreeNode* root){
+	if (!root) return 0;
 
-		if (!root->left) return 1 + minDepth(root->right);
-		if (!root->right) return 1 + minDepth(root->left);
+	if (!root->left) return 1 + minDepth(root->right);
+	if (!root->right) return 1 + minDepth(root->left);
 
-		return 1 + min(minDepth(root->left, root->right));
-	}
+	/*以下描述就是: return 1 + min(minDepth(root->left, root->right));
+		if(root->left) l = minDepth(root->left);
+		if(root->right) r = minDepth(root->>right);
+		return 1+min(l,r);
+	*/
+	return 1 + min(minDepth(root->left, root->right));
+}
+
+
 };
