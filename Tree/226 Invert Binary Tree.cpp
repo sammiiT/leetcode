07c362176ed2ea10d8c9c7deeb0966e6103075f6
sammiiT <dirsing@gmail.com>
2022-11-10 12:16:27 +0800

    Update 226 Invert Binary Tree.cpp

--- Tree/226 Invert Binary Tree.cpp
+++ Tree/226 Invert Binary Tree.cpp
@@ -7,8 +7,9 @@ public:
         if(root->left)  l = invertTree(root->left);
         if(root->right) r = invertTree(root->right);
         
-        root->right = l;
-        root->left = r;
+//        執行步驟在父節點
+        root->right = l;//parent節點運算
+        root->left = r;//parent節點運算
         return root;
     }
     
@@ -36,3 +37,66 @@ public:
         return root;    
     }
 };
+
+
+
+
+//===== 錯誤寫法; 原因 =====
+void helper(TreeNode** left, TreeNode** right){
+    if(!(*left)&&!(*right)) return;
+    TreeNode *tmp = *left;//->left指向的位址
+    *left = *right; //root->left
+    *right = tmp;
+    helper(&((*left)->left),&((*left)->right));//由上往下找,會有null pointer出現, 錯誤
+    helper(&((*right)->left),&((*right)->right));    
+/*
+preorder寫法會造成中間有null pointer形成, 要避免
+(*)用postoer或 inorder概念, 可以避免null pointer.
+(*)要知道前一個node(父node); 可以用postoer或indorer, 因為執行步驟在父節點
+*/
+
+}
+
+//===== 錯誤寫法; 原因 =====
+void helper(TreeNode** left, TreeNode** right){
+    if(!(*left)&&!(*right)) return;
+        
+    TreeNode *tmp = *left;//->left指向的位址
+    *left = *right; //root->left
+    *right = tmp;
+        
+    helper(&((*left)->left),&((*right)->right));
+    helper(&((*left)->right),&((*right)->left));    
+}
+TreeNode* invertTree(TreeNode* root) {
+    if(!root) return root;
+    helper(&root->left,&root->right);
+    return root;
+}
+原因:
+初始:
+4
+2 7
+1 3 6 9
+
+第一次執行:
+4
+7 2    
+6 9 1 3
+    
+第二次執行:
+4
+7 2
+3 1 9 6   => 答案錯誤
+
+
+
+
+
+
+
+
+
+
+
+
