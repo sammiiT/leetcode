84df3e6ffb44653b7cfe4932014018f89c3b15f9
sammiiT <dirsing@gmail.com>
2022-11-12 13:33:43 +0800

    Update 114 Flatten Binary Tree to Linked List.cpp

--- Tree/114 Flatten Binary Tree to Linked List.cpp
+++ Tree/114 Flatten Binary Tree to Linked List.cpp
@@ -17,14 +17,11 @@ public:
     }
 };
 
-
       1
      / \
     2   5
    / \   \
   3   4   6
-
-
 
 1
  \
@@ -37,5 +34,31 @@ public:
         5
          \
           6
+
+//----錯誤; 原因----
+    void helper(TreeNode* root){
+        if(!root) return;
+        if(!root->left && !root->right) return;
+//        if(!root->left && root->right) return;//此描述錯誤
+         if(root->left) helper(root->left);
+        if(root->right) helper(root->right);
+        
+        TreeNode* tmp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        
+        while(root->right){
+            root = root->right;
+        }
+        root->right = tmp;
+    }
+
+if(!root->left && root->right) return; 如果right底下還有child,其底下的child不會做運算(swap).
+
+
+
+
+
+
 
 
