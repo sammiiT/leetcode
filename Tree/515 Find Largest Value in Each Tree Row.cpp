faa70d0e8da5e90b9dec8a2461bfdcf345b33f90
sammiiT <dirsing@gmail.com>
2022-11-19 01:32:08 +0800

    Update 515 Find Largest Value in Each Tree Row.cpp

--- Tree/515 Find Largest Value in Each Tree Row.cpp
+++ Tree/515 Find Largest Value in Each Tree Row.cpp
@@ -18,3 +18,23 @@ public:
         helper(root->right, level+1, res);
     }
 };
+
+void helper(TreeNode* root, int level, vector<int>& res){
+        if(!root) return;
+        
+        if(level==res.size()){
+            res.push_back({});  
+            res[level] = root->val;
+        }else{
+            res[level] = max(res[level],root->val);
+        } 
+        
+        helper(root->left,level+1,res);
+        helper(root->right,level+1,res);
+}   
+vector<int> largestValues(TreeNode* root) {
+        vector<int> res;
+        helper(root,0,res);
+        
+        return res;
+}
