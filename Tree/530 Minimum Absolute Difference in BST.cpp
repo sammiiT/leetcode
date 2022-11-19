bfcb11b82a9373998a08a607a34cf16822237ff2
sammiiT <dirsing@gmail.com>
2022-11-19 08:55:35 +0800

    Update 530 Minimum Absolute Difference in BST.cpp

--- Tree/530 Minimum Absolute Difference in BST.cpp
+++ Tree/530 Minimum Absolute Difference in BST.cpp
@@ -17,7 +17,41 @@ public:
         helper(node->right,res,pre);
         
     }
-    
-    
+ };
 
-};
+//===========================================
+int minvalue(TreeNode *root){
+        if(root->left){
+            return minvalue(root->left);
+        }else{
+            return root->val;
+        }
+}
+int maxvalue(TreeNode *root){
+        if(root->right){
+            return maxvalue(root->right);
+        }else{
+            return root->val;
+        }
+}
+  //每一個parent都往比較最後一個right, left leaf.  
+void helper(TreeNode* root, int& mni){
+        if(!root) return;        
+        int deltal = INT_MAX, deltar = INT_MAX;
+  
+        if(root->left) deltal = abs(root->val - maxvalue(root->left));//比較最後一個leaf
+        if(root->right) deltar = abs(root->val- minvalue(root->right));//比較最後一個leaf
+        
+        mni = min(mni,deltal);
+        mni = min(mni,deltar);
+        
+        helper(root->left,mni);//往下找下一個minimum
+        helper(root->right,mni);//往下找下一個minimum
+}
+    
+int getMinimumDifference(TreeNode* root) {
+        int mni = INT_MAX;
+        helper(root,mni);
+        return mni;
+}
+
