6231496aecb18fa8d39f1de222662c6abc59f383
sammiiT <dirsing@gmail.com>
2022-11-19 13:55:22 +0800

    Update 437 Path Sum III.cpp

--- Tree/437 Path Sum III.cpp
+++ Tree/437 Path Sum III.cpp
@@ -31,6 +31,45 @@ public:
         
         out.pop_back();
     }
-    
-    
 };
+
+//====用減法
+void helper(TreeNode* root, long targetSum, int& count){
+        if(!root) return;        
+        if(targetSum-root->val==0){
+            count++;
+        //    return;//要拿掉, 因為同一條路徑下會重複答案
+        }
+        helper(root->left,targetSum-root->val,count);
+        helper(root->right,targetSum-root->val,count);
+}
+void sum(TreeNode* root, long targetSum, int& count){
+        if(!root) return;
+        helper(root,targetSum, count);
+        sum(root->left,targetSum,count);//錯誤, 前面要加上 if(!root)才會正確
+        sum(root->right,targetSum,count);
+}
+
+//====用加法
+void helper(TreeNode* root, int targetSum, long curSum, int&res){
+      if(!root) return;
+     
+      curSum+=root->val;
+      if(curSum==targetSum) res++;
+      helper(root->left, targetSum, curSum, res);
+      helper(root->right, targetSum, curSum, res);
+//      if((curSum + root->val)==targetSum){ res++; }      
+//      helper(root->left, targetSum, curSum + root->val, res);
+//      helper(root->right, targetSum, curSum + root->val, res);      
+}
+    
+void sum(TreeNode* root, int targetSum, int& count){
+        if(!root) return;
+        helper(root,targetSum, 0, count);
+        sum(root->left,targetSum,count);//錯誤, 前面要加上 if(!root)才會正確
+        sum(root->right,targetSum,count);
+}
+
+
+
+
