afecbedf9900e1533cceefbf44bc5b4181c0f6bd
sammiiT <dirsing@gmail.com>
2022-11-19 21:09:25 +0800

    Update 623 Add One Row to Tree.cpp

--- Tree/623 Add One Row to Tree.cpp
+++ Tree/623 Add One Row to Tree.cpp
@@ -30,3 +30,38 @@ public:
         helper(root->right,level+1,v,d);
     }
 };
+
+//========
+    TreeNode* helper(TreeNode* root, int level, int isleft, int val, int depth){
+//        if(!root) return NULL;
+        if(!root && (level==depth)){//若是在最後一層
+        }else if(!root){
+            return NULL;
+        }
+      
+        
+        if(level==depth && isleft){
+            TreeNode *tmp = new TreeNode(val);
+            tmp->left = root;//連接
+            return tmp;
+        }else if(level==depth && !isleft){
+            TreeNode *tmp = new TreeNode(val);
+            tmp->right = root;//連接
+            return tmp;
+        }
+
+//        if(root->left) root->left = helper(root->left, level+1, 1, val, depth);
+//        if(root->right) root->right = helper(root->right, level+1, 0,val, depth);
+         root->left = helper(root->left, level+1, 1, val, depth);//要最後一個level, 所不能用if(root->left)
+         root->right = helper(root->right, level+1, 0,val, depth);
+
+        
+        
+        return root;
+    }
+//1<=depth<=depth of tree + 1 要考慮第一個,和最後一個     
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        TreeNode* ret;
+        ret = helper(root, 1,1,val,depth);
+        return ret;
+    }
