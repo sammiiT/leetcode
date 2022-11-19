682e16f2944ce2cc9830063177514db3afb69937
sammiiT <dirsing@gmail.com>
2022-11-19 14:17:59 +0800

    Update 637 Average of Levels in Binary Tree.cpp

--- Tree/637 Average of Levels in Binary Tree.cpp
+++ Tree/637 Average of Levels in Binary Tree.cpp
@@ -24,7 +24,32 @@ public:
         helper(root->left,level+1,res);
         helper(root->right,level+1,res);
     }
-    
-    
-    
 };
+
+//================
+void helper(TreeNode* root, int level, vector<vector<int>>& res){
+        if(!root) return;
+        
+        if(level==res.size()) res.push_back({});
+        res[level].push_back(root->val);
+        
+        helper(root->left,level+1,res);
+        helper(root->right,level+1,res);
+}
+    
+vector<double> averageOfLevels(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<double> ret;
+        double sum = 0;
+
+        helper(root, 0, res);
+       
+        for(int i=0;i<res.size();i++){
+            sum = 0;
+            for(int j = 0; j<res[i].size(); j++){
+                sum+=res[i].at(j);
+            }
+            ret.push_back(sum/res[i].size());
+        }
+        return ret;
+}
