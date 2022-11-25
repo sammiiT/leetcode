461211bd3e908da3284ad93c7e37c03e50682bc3
sammiiT <dirsing@gmail.com>
2022-11-25 10:24:17 +0800

    Update 268 Missing Number.cpp

--- array/268 Missing Number.cpp
+++ array/268 Missing Number.cpp
@@ -9,3 +9,16 @@ public:
   
     }
 };
+//========================
+int helper(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        nums.push_back(nums[nums.size()-1]+1);
+        nums.insert(nums.begin(),-1);
+        
+        for(int i = 0; i<nums.size()-1; i++){
+            if(nums[i+1]!=nums[i]+1){
+                return nums[i]+1;
+            }    
+        }
+        return nums[nums.size()-1];
+}
