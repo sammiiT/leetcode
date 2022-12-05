bfcf053a0b7337d53950be4ad68b0c5406d3045a
sammiiT <dirsing@gmail.com>
2022-12-05 22:53:28 +0800

    Update 64 Minimum Path Sum.cpp

--- DynamicProgramming/64 Minimum Path Sum.cpp
+++ DynamicProgramming/64 Minimum Path Sum.cpp
@@ -47,3 +47,50 @@ if()
     
 //(2)Dungeon Game 地牢遊戲 題: dynamic programming   
 };
+
+//=====================================
+    int helper1(vector<vector<int>>& grid){
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
+        
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<grid.size();i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+        }
+        for(int i=1; i<grid[0].size(); i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
+        }
+        
+        for(int i=1; i<grid.size(); i++){
+            for(int j=1; j<grid[0].size(); j++){
+                dp[i][j]=grid[i][j]+min(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp.back().back();
+    }
+    
+    int helper0(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));//dp[m][n];//儲存min sum
+        int sum = 0;
+        
+        for(int i=0; i<m; i++){
+            sum = sum + grid[i][0];
+            dp[i][0] = sum;
+        }
+
+        sum = 0;
+        for(int i=0; i<n; i++){
+            sum = sum + grid[0][i];
+            dp[0][i] = sum;
+        }
+        
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];        
+    }
+
+
