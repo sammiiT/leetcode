c7719135ce8f1d7db2633b08936209cc970e9f42
sammiiT <dirsing@gmail.com>
2022-12-06 12:01:27 +0800

    Create 139. Word Break.cpp

---
+++ DynamicProgramming/139. Word Break.cpp
@@ -0,0 +1,20 @@
+bool helper0(string s, vector<string>& wordDict){
+  unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
+  vector<bool> dp(s.size()+1,0);//+1是為了(i-j)計算, 為了記錄下一個seperation 的start
+  dp[0]=true;
+  
+  for(int i=0; i<dp.size(); i++){
+//j可視為string的每一個index; i是下一個strat的index,且用來運算每一個seperate string的長度    
+    for(int j=0; j<i; j++){
+      if(dp[j] && wordSet.count(s.substr(j,i-j))){
+        dp[i]=true;
+        break;
+      }
+    }
+  }
+  return dp.back();
+}
+
+bool wordBreak(string s, vector<string>& wordDict){
+  return helper0(s,wordDict);
+}
