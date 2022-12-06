5f3640530eed94b2dd018f03b7745e920cdf7951
sammiiT <dirsing@gmail.com>
2022-12-06 12:11:57 +0800

    Update 139. Word Break.cpp

--- DynamicProgramming/139. Word Break.cpp
+++ DynamicProgramming/139. Word Break.cpp
@@ -6,6 +6,7 @@ bool helper0(string s, vector<string>& wordDict){
   for(int i=0; i<dp.size(); i++){
 //j可視為string的每一個index; i是下一個strat的index,且用來運算每一個seperate string的長度    
     for(int j=0; j<i; j++){
+                               //了解s.substr()使用, 間接可以解釋為什麼dp(s.size()+1) 
       if(dp[j] && wordSet.count(s.substr(j,i-j))){
         dp[i]=true;
         break;
