0741bc17d546c5dacdc22d113471d4e70fff9a2e
sammiiT <dirsing@gmail.com>
2022-12-07 21:58:47 +0800

    Update 70 Climbing Stairs.cpp

--- DynamicProgramming/70 Climbing Stairs.cpp
+++ DynamicProgramming/70 Climbing Stairs.cpp
@@ -5,8 +5,8 @@ public:
         
         vector<int> dp(n,0);
         
-        dp[0]=1;//一個階梯有1種
-        dp[1]=2;//兩個階梯有2種
+        dp[0]=1;//index = 0, 就是第1階 =>有一種解法
+        dp[1]=2;//index = 1, 就是第2階 =>有兩種解法
         for(int i=2; i<n; ++i){
             dp[i] = dp[i-1] + dp[i-2];    
         }
