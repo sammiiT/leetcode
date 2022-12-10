110bece16001411fbf73ecf181290701196eb139
sammiiT <dirsing@gmail.com>
2022-12-10 23:48:04 +0800

    Update 56 Merge Intervals.cpp

--- array/56 Merge Intervals.cpp
+++ array/56 Merge Intervals.cpp
@@ -1,4 +1,9 @@
-﻿class Solution {
+//==== 類似題 ====
+57. Insert Interval
+252. Meeting Rooms
+253. Meeting Rooms II
+
+class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals){
         if(intervals.empty()||intervals.size()==1) return intervals;
@@ -48,6 +53,34 @@ public:
         return res;
     }
 };
+//====================================================
+static bool compare(vector<int>& a, vector<int>& b){
+        return a[0]<b[0];
+}
+vector<vector<int>> helper(vector<vector<int>>& intervals) {
+    int n = intervals.size();
+    vector<vector<int>> res;
+    
+    sort(intervals.begin(),intervals.end(),compare);
+    res.push_back(intervals[0]);
+
+    for(int i=1;i<n;i++){
+/*        if(res[i-1][1]>=intervals[i][0]){//合併後, 下一次的res是陣列的最後一個元素
+            res[i-1][1] = intervals[i][1];//所以用i索引的方式錯誤
+        }*/
+        if(res[res.size()-1][1]>=intervals[i][0]){//合併後, 下一次的res是陣列的最後一個元素
+                //res[res.size()-1][1] = intervals[i][1];//所以用i索引的方式錯誤
+                res[res.size()-1][1] = max(res[res.size()-1][1],intervals[i][1]);
+        }
+        else {
+            res.push_back(intervals[i]);
+        }   
+    }
+    return res;
+}
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        return helper(intervals);
+}
 
 
 /*
