a57967cca6bb0a971c63a7c2e1f07b9777e67a69
sammiiT <dirsing@gmail.com>
2022-12-11 10:52:15 +0800

    Create 1118. Number of Days in a Month.cpp

---
+++ array/1118. Number of Days in a Month.cpp
@@ -0,0 +1,13 @@
+int numberOfDays(int Y, int M) {
+  if(M==1||M==3||M==5||M==7||M==8||M==10||M==12){
+    return 31;
+  }else if(M==4||M==6||M==9||M==11){
+    return 30;
+  }else{
+    if((Y%4==0 && Y%100!=0)||Y%400==0){//閏年條件
+      return 29;
+    }else{
+      return 28;
+    }
+  }
+}
