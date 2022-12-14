eb130cdf2f2892555101adf9360555707e566449
sammiiT <dirsing@gmail.com>
2022-12-14 23:11:25 +0800

    Update 125. Valid Palindrome.cpp

--- array/125. Valid Palindrome.cpp
+++ array/125. Valid Palindrome.cpp
@@ -4,7 +4,12 @@
         while(left<right){
             if(!isAlphaNum(s[left])) left++;
             else if(!isAlphaNum(s[right])) right--;
-            else if((s[left]+32-'a')%32!=(s[right]+32-'a')%32) return false;//將capital character轉換為小寫
+            else if((s[left]+32-'a')%32!=(s[right]+32-'a')%32) return false;
+/*
++32 是因為A的數值是65, 如果減去'a'=97會變成負值
+'A'-'a' = 65-97=(-32)
+加上32之後 'A'-'a'= 0; 大寫減去'a',就不會變成負數
+*/
             else {
                 left++; right--;//遇到非字母的character
             }
