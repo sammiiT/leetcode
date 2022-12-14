51be41ce49b134689ea198f982ae7f55968ec1e1
sammiiT <dirsing@gmail.com>
2022-12-14 22:02:53 +0800

    Update 9. Palindrome Number.cpp

--- array/9. Palindrome Number.cpp
+++ array/9. Palindrome Number.cpp
@@ -8,7 +8,7 @@ bool helper2(int x){
         long y = 0;
         if(x<0) return false;
         while(count>0){
-            y = y*10 + count%10;
+            y = y*10 + count%10;//將每一次最低位的數,往前推;最後palindrome
             count/=10;        
         }
         return (y==x)?true:false;
@@ -16,7 +16,6 @@ bool helper2(int x){
 
 
 bool helper0(int x){
-        
         long i = x, j=0;//int會出錯,要用long
         int div = 1;//計算order
         if(x<0) return false;
@@ -42,18 +41,18 @@ bool helper0(int x){
         while(x/div >= 10) div = div*10;
         
         while(div>0){//loop {
-            int left =  x/div;
-            int right = x%10;
-            if(left!=right) return false;
+            int left =  x/div;//最大位的數
+            int right = x%10;//最小位的數
+            if(left!=right) return false;//如果兩個不相等,回傳false
             x = x%div;//移除最高位
             x = x/10;//移除最低位
-            div = div/100;
+            div = div/100;//移除兩位,所以要除100
         }
         return true;
     }
     
     
-    bool isPalindrome(int x) {
+bool isPalindrome(int x) {
         return helper0(x);
 //        return helper1(x);
-    }
+}
