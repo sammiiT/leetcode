6e8d0cc012c6a2e4ac9be7cb508522e849822af4
sammiiT <dirsing@gmail.com>
2022-12-16 11:21:32 +0800

    Update 41. First Missing Positive.cpp

--- array/41. First Missing Positive.cpp
+++ array/41. First Missing Positive.cpp
@@ -50,12 +50,12 @@ int helper1(vector<int>& nums){
 //====思路
 
 //===
-/*int helper2(vector<int>& nums) {
+int helper2(vector<int>& nums) {
          sort(nums.begin(), nums.end());
         int i = 1;
         for(int x: nums) if(x == i) i++;
         return i;        
-}*/
+}
 
 int firstMissingPositive(vector<int>& nums) {
         return helper0(nums);    
