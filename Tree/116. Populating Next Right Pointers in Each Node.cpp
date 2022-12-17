e841cfd973cba258036f6ec25ef6e60bfc90673b
sammiiT <dirsing@gmail.com>
2022-12-17 23:19:30 +0800

    Create 116. Populating Next Right Pointers in Each Node.cpp

---
+++ Tree/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,28 @@
+//====類似題===
+117. Populating Next Right Pointers in Each Node II
+
+//===思路===
+1. 記錄每一level的所有node; 此資料會形成一個vector<vector<Node*>>的陣列
+2. 遍歷每一階層的陣列, 並將陣列中的每一個Node的next指向下一個節點
+//=====
+
+void helper0(Node* root, int level, vector<vector<Node*>>& res){
+    if(!root) return;
+    if(level==res.size()) res.push_back({});
+    res[level].push_back(root);
+    helper0(root->left, level+1, res);
+    helper0(root->right, level+1,res);    
+}
+
+
+Node* connect(Node* root) {
+        vector<vector<Node*>> res;
+        helper0(root,0,res);
+        for(int i=0; i<res.size(); i++){
+            for(int j=0; j<res[i].size(); j++){
+                if(j==res[i].size()-1) res[i][j]->next=NULL;
+                else res[i][j]->next=res[i][j+1];
+            }
+        }    
+        return root;    
+}
