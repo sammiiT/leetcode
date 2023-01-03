93f877adcdcb6540011a3af6853ee42f1c88f17a
sammiiT <dirsing@gmail.com>
2023-01-03 22:13:12 +0800

    Update Remove Element.cpp

--- array/Remove Element.cpp
+++ array/Remove Element.cpp
@@ -1,7 +1,20 @@
+//===類似題===
+28. Find the Index of the First Occurrence in a String
+2244. Minimum Rounds to Complete All Tasks
+1213. Intersection of Three Sorted Arrays
+2491. Divide Players Into Teams of Equal Skill
+//===思路===
+nums = [3,2,2,3], val = 3    
+nums = [0,1,2,2,3,0,4,2], val = 2    
+1.設定一所引 pre=-1; 初始值為-1
+2. 因為要將等於val的數值刪除, 並將後面的數值往前移
+3. 當遇到不等於val的數, pre+1更新, 並將不等於val的數指派給pre位置
+4. 最後回傳pre+1 就是刪除val的總個數.    
+    
+//===
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val){
-        
         int pre =-1;
         for(int i=0;i<nums.size();++i){
             if(nums[i]!=val){
@@ -23,3 +36,20 @@ public:
         return cur;
     }
 };
+
+//===思路====
+1.遍歷陣列
+2.當數值等於val, 則erase; 當不等於 index+1
+3.最後回傳陣列的size()
+//===
+int helper0(vector<int>& nums, int val){
+    int i=0;
+    while(i<nums.size()){
+        if(nums[i]==val){
+            nums.erase(nums.begin()+i);
+        }else{
+            i++;
+        }
+    }
+    return nums.size();
+}
