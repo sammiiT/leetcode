79923c2987352707e709ea6888b625b2383e6fb0
sammiiT <dirsing@gmail.com>
2023-01-05 00:17:46 +0800

    Create 876. Middle of the Linked List.cpp

---
+++ LinkedList/876. Middle of the Linked List.cpp
@@ -0,0 +1,38 @@
+//===類似題
+877. Stone Game
+1498. Number of Subsequences That Satisfy the Given Sum Condition
+986. Interval List Intersections
+2300. Successful Pairs of Spells and Potions
+
+//===思路===
+(*)while(cur&&cur->next)代表: 
+1. 至少有一個節點
+2. cur可以走兩步
+
+(*)cur一次走兩步, pre一次走一步
+
+(*)根據提議搜尋second middle
+first middle:
+*->o->o->o->o->x
+hdr.next = head;
+pre = cur =hdr;
+
+second middle:
+*->o->o->o->o->x
+pre = cur= head;
+
+//====
+ListNode* helper0(ListNode* head){
+  LIstNode* pre,*cur;
+  pre=cur=head;
+  while(cur&&cur->next){
+    pre=pre->next;
+    cur=cur->next->next;
+  }
+  return pre;
+}
+
+
+ListNode* middleNode(ListNode* head) {
+        
+}
