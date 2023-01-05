026d40d234ea6fa2e792d1509091542c84a78b5e
sammiiT <dirsing@gmail.com>
2023-01-05 22:17:06 +0800

    Update 142 Linked List Cycle II.cpp

--- LinkedList/142 Linked List Cycle II.cpp
+++ LinkedList/142 Linked List Cycle II.cpp
@@ -1,3 +1,44 @@
+//====類似題====
+143. Reorder List
+432. All O`one Data Structure
+1442. Count Triplets That Can Form Two Arrays of Equal XOR
+1124. Longest Well-Performing Interval
+
+//===思路====
+(*)先判斷list有沒有cycle
+(*)有cycle之後,判斷交點
+1. 用快慢指標來判斷; 快指標,一次走二步;慢指標,一次走一步
+2. 如果有cycle, 則快指標==慢指標 代表有cycle; 此時f==r並不是交點
+3.一個指標重新指向head, 和另外一個指標一次走一步, 最後相等的才是交點
+
+//====
+ListNode* helper(ListNode* head){
+    ListNode *f,*r;
+    int intersection =0;
+    f=r=head;
+    while(r&&r->next){
+        f=f->next;
+        r=r->next->next;
+        if(f==r){
+            intersection = 1;
+            break;
+        }
+    }
+        
+    f=head;
+    if(intersection){
+        while(f!=r){
+            f=f->next;
+            r=r->next;
+        }
+    }
+    return intersection?f:NULL;        
+}
+ListNode *detectCycle(ListNode *head) {
+        return helper(head);
+}
+
+//========
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
