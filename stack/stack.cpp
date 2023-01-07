5938171041259b7d562d6ac4795692b1f1fe8bc7
sammiiT <dirsing@gmail.com>
2023-01-07 12:01:33 +0800

    Update stack.cpp

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -9,8 +9,6 @@ void pop()
 int top()
 bool isEmpty()
 int getSize()
-
-
 //==================  
 struct stack{
     int top;//top index
@@ -47,7 +45,17 @@ int top(struct stack* stk){
 int getSize(struct stack* stk){
     return stk->top-(-1);
 }
-
+(*)上述做法缺陷,不能push進negative數值,因為 top()函式會有機會回傳(-1)
+除非每次在執行top()之前, 先執行一次isEmpty(),判斷是否為空,再做top的動作 
+    
+(*)在一對stack的遍歷, 之前都會執行stk.empty(),判斷是否可以從stack中取值; 如果沒有先做判斷,就直接執行top動作,會出錯
+while(!stk.empty()){
+    int t = stk.top();
+    stk.pop();
+}
+//如果stk已經是empty()狀態, 
+int t = stk.top(); //執行會出錯
+    
 /****************************************************
  先賦值,再位移 => 不好算,因為top的位址都沒有新的數值 
 ****************************************************/
@@ -96,3 +104,30 @@ int main()
     stack_remove(&stk);    
     return 0;
 }
+
+/****************************************************
+以stack來實作queue
+****************************************************/
+(*)stack操作
+void stack_init(struct stack* stk, int size);
+void stack_remove(struct stack *stk);
+void push(struct stack* stk, int val);
+bool isEmpty(struct stack* stk);
+void pop(struct stack* stk);
+int top(struct stack* stk);
+int getSize(struct stack* stk);
+
+(*)queue操作
+
+int push(int val);
+int pop();
+int getFront();
+int getBack();
+int getSize();
+bool isEmpty();
+bool isFull();
+
+
+
+
+
