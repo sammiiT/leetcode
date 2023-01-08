d5345bae6e86266adfb9d5f03cefd94fb9bc7490
sammiiT <dirsing@gmail.com>
2023-01-08 16:29:19 +0800

    Create 20. Valid Parentheses.cpp

---
+++ stack/20. Valid Parentheses.cpp
@@ -0,0 +1,56 @@
+//===類似題===
+21. Merge Two Sorted Lists
+22. Generate Parentheses
+32. Longest Valid Parentheses
+301. Remove Invalid Parentheses
+//===思路====
+(*)用stack
+1.(,[,{和},])一定要對應出現 ()[]{}, {[()]}
+2.出現 ([{就push進 stack
+3.非 ([{就和stack.top做比較, 如果有成對, 就stack.pop
+  如果不成對, 即return false;
+       
+4.最後比較完, 還有一條件判斷, 因為有[[[ ]] 狀況發生
+  stk.empty()?true:false;
+5.  stack中的element都清空, 才是滿足條件的解.
+                      
+//=====
+   bool helper0(string s){
+        stack<char> stk;
+        for(int i=0; i<s.size(); i++){            
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){//push
+                stk.push(s[i]);
+            }else{//check
+                if(stk.empty()) return false;
+                if((stk.top()=='('&&s[i]==')')||(stk.top()=='['&&s[i]==']')||(stk.top()=='{'&&s[i]=='}')) 
+                    stk.pop();
+                //else stk.push(s[i]); 
+                else return false;
+            }
+        }
+        return stk.empty()?true:false;
+     }
+
+bool helper1(string s){
+    stack<char> stk;
+    
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='('||s[i]=='['||s[i]=='{') stk.push(s[i]);
+        else {//),],}
+            
+            if(!stk.empty() && ((s[i]==')'&&stk.top()=='(')||(s[i]=='}'&&stk.top()=='{')||(s[i]==']'&&stk.top()=='[')) ){ 
+                stk.pop();
+            }
+            else {//stk.empty()
+                return false;
+            }
+        }
+    }
+//    return true;//[[[ ]]若出現此排列,會錯誤
+    return stk.empty()?true:false;
+}
+
+    bool isValid(string s) {
+        return helper1(s);
+//        return helper0(s);
+    }
