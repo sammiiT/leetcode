eb6a5a74045784f7f7f0f7cc6e05fc3a36236b56
sammiiT <dirsing@gmail.com>
2023-01-09 22:15:45 +0800

    Update 1472. Design Browser History.cpp

--- stack/1472. Design Browser History.cpp
+++ stack/1472. Design Browser History.cpp
@@ -53,3 +53,33 @@ private:
     vector<string> bstk;
     string cur_page;    
 };
+
+
+//===思路2====
+class BrowserHistory {
+public:
+  BrowserHistory(string homepage) : 
+    index_(0) {
+    urls_.push_back(std::move(homepage));    
+  }
+ 
+  void visit(string url) {
+    while (urls_.size() > index_ + 1)
+      urls_.pop_back();
+    ++index_;
+    urls_.push_back(std::move(url));
+  }
+ 
+  string back(int steps) {    
+    index_ = max(index_ - steps, 0);
+    return urls_[index_];
+  }
+ 
+  string forward(int steps) {
+    index_ = min(index_ + steps, static_cast<int>(urls_.size()) - 1);  
+    return urls_[index_];
+  }
+private:
+  int index_;
+  vector<string> urls_;
+};
