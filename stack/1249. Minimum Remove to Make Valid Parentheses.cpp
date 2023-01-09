81b71809d18c115d821c7b5fd2f81b4464492138
sammiiT <dirsing@gmail.com>
2023-01-09 23:34:00 +0800

    Update 1249. Minimum Remove to Make Valid Parentheses.cpp

--- stack/1249. Minimum Remove to Make Valid Parentheses.cpp
+++ stack/1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -75,3 +75,22 @@ string minRemoveToMakeValid(string s) {
 
         return ans;
     }
+//===思路3===
+    string minRemoveToMakeValid(string s) {
+        string res;
+        stack<int> st;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') st.push(i);
+            else if (s[i] == ')') {
+                if (st.empty()) s[i] = '*';
+                else st.pop();
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '*'; st.pop();
+        }
+        for (char c : s) {
+            if (c != '*') res += c;
+        }
+        return res;
+    }
