15f825ab4613d9712a53a3a1acda517aeac5e00f
sammiiT <dirsing@gmail.com>
2023-01-11 23:08:39 +0800

    Create 146. LRU Cache.cpp

---
+++ hashmap_hashset/146. LRU Cache.cpp
@@ -0,0 +1,52 @@
+//===類似題===
+147. Insertion Sort List
+460. LFU Cache
+588. Design In-Memory File System
+604. Design Compressed String Iterator
+
+//===思路====
+(*) list 模擬 LRU cache
+list可以push_front(), push_back(),erase(),back()
+list還可以 splice; 剪list 插入list的任意位置
+
+1.概念: list中最後一個位置, 就是最不常用到的cache,
+每次對list做access(put,get) , 都把其對應的iterator放到list最前面
+不用紀錄每一個key的使用次數, 有用到, 就把對應的iterator放在最前面; 最少用到的一定是在
+
+2.定義list<pair<int,int>>//key, value
+
+3.定義unordered_map,紀錄key和list的iterator
+unordered_map<int,list<pair<int,int>>::iterator>//key, list的iterator
+
+
+//========
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    int get(int key) {
+        auto it = m.find(key);
+        if(it==m.end()) return -1;
+        l.splice(l.begin(), l, it->second);
+        return it->second->second;
+    }
+    void put(int key, int value) {
+        auto it = m.find(key);
+        if(it!=m.end()) l.erase(it->second);//remove for update
+        l.push_front(make_pair(key,value));
+        m[key]=l.begin();
+        if(m.size()>cap){
+            int k = l.back().first;
+            //int k = l.rbegin()->first;
+            l.pop_back();
+            m.erase(k);
+        }
+    }
+private:
+    int cap;
+    list<pair<int,int>> l;//cache
+    unordered_map<int,list<pair<int,int>>::iterator> m; //key, interator from "l"
+};
+
+
