643b852808bf8879d311caeb4f9e89a42ff2f50f
sammiiT <dirsing@gmail.com>
2023-01-14 14:33:09 +0800

    Update readme.cpp

--- hashmap_hashset/readme.cpp
+++ hashmap_hashset/readme.cpp
@@ -15,5 +15,44 @@
 .lower_bound(elem) =>回傳>=elem的第一個iterator
 .upper_bound(elem) =>回傳>elem的第一個iterator
 .equal_range(elem) =>回傳==elem的區間
-  
- //=== unordered_set ====
+
+int main(){   
+    multiset<int> st;
+    st.insert(7);
+    st.insert(4);
+    st.insert(3);
+    st.insert(9);
+    st.insert(8);
+    for(multiset<int>::iterator it = st.begin(); it!=st.end(); it++){
+        cout<<*it<<" ";
+    } cout<<endl;
+    
+    pair<multiset<int>::iterator,multiset<int>::iterator> ret = st.equal_range(3);//回傳一個iterator的pair, first是初始位置,second的位址是最後一個的下一個
+   
+    cout<<*(ret.first)<<" "<<*(ret.second)<<endl; //3,4 ; 4是最後一個的下一個
+//    st.erase(ret.first);
+    st.erase(ret.first,ret.second);
+    
+    while(!st.empty()){
+        cout<<*st.begin()<<" ";
+        st.erase(st.begin());
+    }cout<<endl;
+    cout<<st.size()<<endl;
+    return 0;
+}
+
+int main ()
+{
+  int myints[]= {77,30,16,2,30,30};
+                                                    
+  std::multiset<int> mymultiset(myints, myints+6); // 2 16 30 30 30 77
+/*
++6 是把end()也考慮進去
+77,  30,  16,  2,  30,  30 ,   x   
+^                              ^  
+|---myints                     |---end(); 就是myints+6的位置
+*/                                                    
+                                                    
+  std::pair<It,It> ret = mymultiset.equal_range(30); //      ^        ^
+}
+//=== unordered_set ====
