ffd1f7110d000f8188b71e50bf69ffb71bde83d8
sammiiT <dirsing@gmail.com>
2023-01-15 17:28:53 +0800

    Update 295. Find Median from Data Stream.cpp

--- priority_queue/295. Find Median from Data Stream.cpp
+++ priority_queue/295. Find Median from Data Stream.cpp
@@ -1,14 +1,39 @@
 //===類似題===
+296. Best Meeting Point
+480. Sliding Window Median
+1825. Finding MK Average
+2102. Sequentially Ordinal Rank Tracker
 
 //===思路===
+(*)將數列分成兩部分,較大數列是一個partition, 另外一個partition是較小的數
+(*)用兩個priority_queue來區這兩個partition.
 
+當small_q的size大於 large_q的size, 代表全部是奇數個, 所以取small的top即可
+當small_q的size等於large_1的size, 代表全部是偶數個, 所以取(small.top()-large.top())/; 
+large.top()是加上負號的數值,所以top的數, 是絕對值較小的數
 //======
+class MedianFinder {
+public:
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        
+        if(small.size()<large.size()){
+            small.push(-large.top());
+            large.pop();
+        }
+    }
 
-
-
-
-
-
+    double findMedian() {
+        return small.size()>large.size()? small.top(): (double)(small.top()-large.top())/2;
+    }
+private:
+    priority_queue<int> small, large;
+};
 
 
 //===fail: time limit exceed ====
