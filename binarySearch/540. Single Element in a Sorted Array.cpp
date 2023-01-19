5d0bb68a15ed71f7c2c0e2f0c21e32d7a2ade318
sammiiT <dirsing@gmail.com>
2023-01-19 22:42:44 +0800

    Create 540. Single Element in a Sorted Array.cpp

---
+++ binarySearch/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,49 @@
+//===類似題===
+541. Reverse String II
+2250. Count Number of Rectangles Containing Each Point
+1872. Stone Game VIII
+1470. Shuffle the Array
+//===思路===
+(*)數列一定是奇數個
+(*)如果nums[m]==nums[m+1]
+左右兩邊是奇數個, 則single一定發生在左半邊
+左右兩邊是偶數個, single在右半邊
+
+nums[m]==nums[m-1]
+左右兩邊是奇數個, 則single一定發生在右半邊
+左右兩邊是偶數個, single在左半邊
+
+      V
+  1 1 2 2 3    =>左右兩邊,偶數個=>一定發生在"右"半邊
+      V
+0 1 1 2 2 3 3  =>左右兩邊,奇數個=>一定發生在"左"半邊
+  
+
+      V
+  1 2 2 3 3    =>左右兩邊,偶數個 =>一定發生在"左"半邊
+      V
+1 1 2 2 3 3 4  =>左右兩邊,奇數個 =>一定發生在"右"半邊   
+
+      V
+  1 1 2 3 3   =>發生在中間點
+
+//====
+int helper0(vector<int>& nums){
+    int l = 0, r = nums.size()-1, n = nums.size();
+    while(l<r){
+        int m = l +(r-l)/2;
+        if(nums[m]==nums[m+1]){
+            if((n-1-m)%2) r = m;
+            else l = m+1;
+        } else {
+            if(m==0 || nums[m]!=nums[m-1]) return nums[m];
+            if((n-1-m)%2) l = m+1;
+            else r = m;
+        }
+    }
+    return nums[r];
+}
+
+int singleNonDuplicate(vector<int>& nums) {
+    return helper0(nums);
+}
