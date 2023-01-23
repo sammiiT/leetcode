cac124f13a00ed766dac06e0a7f4259989afc6cc
sammiiT <dirsing@gmail.com>
2023-01-23 22:58:12 +0800

    Update 151. Reverse Words in a String.cpp

--- array/151. Reverse Words in a String.cpp
+++ array/151. Reverse Words in a String.cpp
@@ -10,7 +10,8 @@ string helper0(string s){
         string t;
 
         for(int i=0;i<=s.size();i++){
-            if(s[i]==' '||i==s.size()){//如果遇到space或i等於s.size();超過index 
+//            if(s[i]==' '||i==s.size()){//如果遇到space或i等於s.size();超過index 
+             if(i==s.size()||s[i]==' '){//要先判斷i==s.size(), 因為超出範圍s[i]==' '的s[i]不存在            
                 if(t.size()){          //將紀錄的string記錄至vector<string>中
                     res.push_back(t);
                     t.clear();        
@@ -19,7 +20,6 @@ string helper0(string s){
                 t+=s[i];
             }        
         }
-
         t.clear();
         for(int i=res.size()-1; i>=0; --i){//reserve
             t+=res[i];
@@ -32,7 +32,8 @@ string helper1(string s){
         string r;
         string t;
         for(int i=0; i<=s.size(); i++){
-            if(s[i]==' '|| i==s.size()){
+//            if(s[i]==' '|| i==s.size()){
+            if(i==s.size()||s[i]==' '){//要先判斷i==s.size(), 因為超出範圍s[i]==' '的s[i]不存在  
                 if(t.size()){
                     (r.size()==0)?r.insert(0,t):r.insert(0,t+' ');        
                     t.clear();
