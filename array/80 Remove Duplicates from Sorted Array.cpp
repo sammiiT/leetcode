ee57d4dc22d7ce546aedb316190e41f3b1ca348c
sammiiT <dirsing@gmail.com>
2023-01-24 15:18:07 +0800

    Update 80 Remove Duplicates from Sorted Array.cpp

--- array/80 Remove Duplicates from Sorted Array.cpp
+++ array/80 Remove Duplicates from Sorted Array.cpp
@@ -1,3 +1,34 @@
+//===類似題===
+27. Remove Element
+2460. Apply Operations to an Array
+
+//===思路===
+
+
+//===
+int helper1(vector<int>& nums){
+    vector<int> t;
+    t.push_back(nums[0]);
+
+    for(int i=1; i<nums.size(); i++){
+        if(nums[i]!=nums[i-1]) t.push_back(nums[i]);
+    }
+    nums = t;//將nums remove為題意
+    return t.size();
+}
+int helper0(vector<int>& nums){
+    int j=0;
+    for(int i=1; i<nums.size();i++){
+        if(nums[i]!=nums[i-1]){
+            j++;//不相等,則update index, 並將值填入
+            nums[j]=nums[i];//值填入
+        }
+    }
+    return j+1;//回傳個數
+}
+
+
+//=======
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
