102dccc1e65eb13ecf2a7f3026630d2609f316b5
sammiiT <dirsing@gmail.com>
2023-01-28 13:59:10 +0800

    Update 102 Binary Tree Level Order Traversal.cpp

--- Tree/102 Binary Tree Level Order Traversal.cpp
+++ Tree/102 Binary Tree Level Order Traversal.cpp
@@ -1,3 +1,14 @@
+//===類似題===
+103. Binary Tree Zigzag Level Order Traversal
+314. Binary Tree Vertical Order Traversal
+993. Cousins in Binary Tree
+2471. Minimum Number of Operations to Sort a Binary Tree by Level
+//===思路===
+(*) tree level_order 遍歷
+1. 每一個level都是一個陣列
+2. 每進入新的下一層, 判斷level是否等於res.size(), 如果等於,代表要新增一個陣列
+
+//======
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
