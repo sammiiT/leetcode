cadf92878dbcc7f94ef8a90998a7859561161428
sammiiT <dirsing@gmail.com>
2023-01-31 23:57:52 +0800

    Update 543 Diameter of Binary Tree.cpp

--- Tree/543 Diameter of Binary Tree.cpp
+++ Tree/543 Diameter of Binary Tree.cpp
@@ -1,4 +1,34 @@
-﻿class Solution {
+//===類似題===
+544. Output Contest Matches
+1522. Diameter of N-Ary Tree
+2246. Longest Path With Different Adjacent Characters
+//===思路===
+(*)post-order算法
+-每一次運算會回傳左節點的長度,和右節點的長度
+-運算完之後, 比較左右長度,回傳較長的那一個; return max(l,r)+1; 加1的目的是當下那一層的節點,也算一個個數, 回傳給上一層
+-最長diameter是(r+l); 每一層都會計算一次(r+l)
+//=====
+int helper0(TreeNode* root, int& res){
+    if(!root) return 0;
+    int l = 0,r = 0;
+    
+    if(root->left) l = helper0(root->left, res);
+    if(root->right) r = helper0(root->right, res);
+
+//回到原來的階層,比較子路徑L和R,哪一個比較長
+    res = max(res, 1+r+l);
+    return (l>r)?(l+1):(r+1);
+  //  return 1 + max(l,r);//這一行描述也可以
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+    int res = 0;
+    helper0(root,res);
+    return res-1;//減1 是因為不包含"解"最上面的父節點   
+}
+
+//=======================
+class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int res = 0;
