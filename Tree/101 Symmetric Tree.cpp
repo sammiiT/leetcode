025602d0fc14175b15cba48f3c2542a290ef13c2
sammiiT <dirsing@gmail.com>
2023-02-01 23:45:48 +0800

    Update 101 Symmetric Tree.cpp

--- Tree/101 Symmetric Tree.cpp
+++ Tree/101 Symmetric Tree.cpp
@@ -1,3 +1,16 @@
+//===類似題===
+102. Binary Tree Level Order Traversal
+1254. Number of Closed Islands
+1236. Web Crawler
+1197. Minimum Knight Moves
+//===思路===
+(*)算法有點像pre-order
+(*)先將兩個節點的做比較,
+-如果兩個不相等,則直接回傳false,這樣就不會繼續往下一個節點找=>斷點
+-不相等的情形包括: 一個節點存在,一個節點不存在; 兩個節點都存在,但數值不相等
+
+(*)如果能比較到最後一個節點,且兩節點都是NULL, 則回傳true
+//====
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
