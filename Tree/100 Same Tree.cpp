c6cd2bb4786bc52d08b808eb01abb55cbae8ace1
sammiiT <dirsing@gmail.com>
2023-02-02 21:22:03 +0800

    Update 100 Same Tree.cpp

--- Tree/100 Same Tree.cpp
+++ Tree/100 Same Tree.cpp
@@ -1,3 +1,16 @@
+//===類似題===
+101. Symmetric Tree
+2368. Reachable Nodes With Restrictions
+301. Remove Invalid Parentheses
+959. Regions Cut By Slashes
+
+//===思路===
+(*)有點像pre-order概念
+1.在當下的節點就判斷是否相等,
+-如果相等,就繼續往下一層找, 最後一層是兩個節點都是NULL, 如果兩個節點都是NULL則回傳true
+-如果不相等, 就回傳false,從當下那一層截斷, 不往下層找
+    
+//=====
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
