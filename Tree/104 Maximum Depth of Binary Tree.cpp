a14786008318bdf9d0187ac45f2e74101b06f554
sammiiT <dirsing@gmail.com>
2023-02-02 23:54:05 +0800

    Update 104 Maximum Depth of Binary Tree.cpp

--- Tree/104 Maximum Depth of Binary Tree.cpp
+++ Tree/104 Maximum Depth of Binary Tree.cpp
@@ -1,3 +1,18 @@
+//===類似題===
+105. Construct Binary Tree from Preorder and Inorder Traversal
+1376. Time Needed to Inform All Employees
+2385. Amount of Time for Binary Tree to Be Infected
+2458. Height of Binary Tree After Subtree Removal Queries
+//===思路===
+(*)post-order解法  (DFS)
+-每一層節點,都會接收到下一層的left和right的最長長度
+-得到left和right之後, 在當下那一層比較left和right長度,回傳比較長的那一個給上層
+-回傳較長的那一個給上層,同時還要加1, 代表當下層的節點數
+
+(*)第一次走到最深的節點,只會回傳1給上層,因為left和right都是NULL,所以只會有一個節點往上層傳
+
+
+//==========
 class Solution {
 public:
     //post order
