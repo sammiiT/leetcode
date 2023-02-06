55e54752d03ae2db48c9cb7fe2609947218563cb
sammiiT <dirsing@gmail.com>
2023-02-07 00:32:57 +0800

    Update 530 Minimum Absolute Difference in BST.cpp

--- Tree/530 Minimum Absolute Difference in BST.cpp
+++ Tree/530 Minimum Absolute Difference in BST.cpp
@@ -1,3 +1,17 @@
+//===類似題===
+531. Lonely Pixel I
+765. Couples Holding Hands
+934. Shortest Bridge
+558. Logical OR of Two Binary Grids Represented as Quad-Trees
+//===思路===
+(*)pre-order算法 or inorder算法 都可以
+- pre-order解法思路很直覺
+
+- inorder思路很有技巧性;
+--利用inorder特性可以有pre-order算法中求minvalue和maxvalue的效果
+
+
+//=====inorder算法==
 class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
@@ -19,7 +33,7 @@ public:
     }
  };
 
-//===========================================
+//====pre-order算法======
 int minvalue(TreeNode *root){
         if(root->left){
             return minvalue(root->left);
@@ -54,4 +68,35 @@ int getMinimumDifference(TreeNode* root) {
         helper(root,mni);
         return mni;
 }
+
+//===failed===
+/*[1,0,48,null,null,12,49]會錯誤
+int helper(TreeNode* root, int& res){
+    if(!root) return 0;不能將NULL節點作考慮
+
+    int l = helper(root->left,res);
+    int r = helper(root->right,res);
+
+    res = min(res,abs(root->val-l));
+    res = min(res,abs(root->val-r));
+    return root->val;
+}*/
+
+/*
+[236,104,701,null,227,null,911]會錯誤
+int helper(TreeNode* root, int& res){
+    if(!root) return 0;
+    int l,r;
+    if(root->left) { 
+        l = helper(root->left,res);
+        res = min(res,abs(root->val-l));//要考慮left節點路徑中最大的數值
+    }
+    if(root->right){
+        r = helper(root->right,res);
+        res = min(res,abs(root->val-r));//要考慮right節點路徑中最小的數值
+    }
+    return root->val;
+}*/
+
+
 
