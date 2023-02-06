8d410cf9cd8ba087ca7a1f2b92841ed01cd907c4
sammiiT <dirsing@gmail.com>
2023-02-06 23:30:13 +0800

    Update 515 Find Largest Value in Each Tree Row.cpp

--- Tree/515 Find Largest Value in Each Tree Row.cpp
+++ Tree/515 Find Largest Value in Each Tree Row.cpp
@@ -1,3 +1,14 @@
+//===類似題===
+516. Longest Palindromic Subsequence
+1202. Smallest String With Swaps
+2328. Number of Increasing Paths in a Grid
+841. Keys and Rooms
+//===思路===
+(*)level-order trasversal (DFS)
+(*)將之前2D換成1維陣列
+-每次到相同level,則用max(x,y)來決定當下的數值
+-最後求出的1維陣列,即為解
+//=====
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
@@ -9,9 +20,7 @@ public:
     
     void helper(TreeNode *root, int level, vector<int>& res){
         if(!root) return;
-        
         if(res.size()==level) res.push_back(INT_MIN);
-        
         res[level] = max(res[level],root->val);
         
         helper(root->left, level+1, res);
