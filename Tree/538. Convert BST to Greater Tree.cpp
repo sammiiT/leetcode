87d23bc4cc8a356a3ed23facf645e9b96e47f3bb
sammiiT <dirsing@gmail.com>
2023-02-07 22:00:30 +0800

    Update and rename 538. Convert BST to Greater Tree to 538. Convert BST to Greater Tree.cpp

--- Tree/538. Convert BST to Greater Tree.cpp
+++ Tree/538. Convert BST to Greater Tree.cpp
@@ -1,4 +1,16 @@
-    void inorder(TreeNode* root, int& sum){
+//===類似題===
+539. Minimum Time Difference
+1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+1810. Minimum Path Cost in a Hidden Grid
+1740. Find Distance in a Binary Tree
+
+//===思路====
+(*)inorder (DFS)
+ 1. inorder解法,但要先遍歷right, 再left
+ 2. 每次紀錄節點的累加,並將累加數值指派給當下節點
+ 3. 遍歷完, 即是解
+//=======
+void inorder(TreeNode* root, int& sum){
         if(!root) return;
         
         inorder(root->left, sum);
