45d16a1975eb3d7bbad5adc0d8ab0ad05e739ed6
sammiiT <dirsing@gmail.com>
2023-02-08 00:11:43 +0800

    Update 623 Add One Row to Tree.cpp

--- Tree/623 Add One Row to Tree.cpp
+++ Tree/623 Add One Row to Tree.cpp
@@ -1,4 +1,81 @@
-﻿class Solution {
+//===類似題===
+624. Maximum Distance in Arrays
+993. Cousins in Binary Tree
+1650. Lowest Common Ancestor of a Binary Tree III
+2049. Count Nodes With the Highest Score
+//===思路1==== 
+
+//======
+TreeNode* helper1(TreeNode* root, int level, int val, int depth){
+    if(!root) return root;
+    if(level==depth-1){
+        TreeNode* l =new TreeNode(val);
+        TreeNode* r =new TreeNode(val);
+        l->left = root->left;
+        r->right = root->right;
+        root->left = l;
+        root->right = r;
+        return root;
+    }    
+/*    if(level==depth-1){
+        TreeNode *l =NULL,*r=NULL;
+        if(root->left){
+            l = new TreeNode(val);
+            l->left = root->left;
+        }
+        if(root->right){
+            r = new TreeNode(val);    
+            r->right = root->right;
+        }
+        root->left = l;
+        root->right = r;
+        return root;
+    }*/
+    root->left = helper1(root->left,level+1,val,depth);
+    root->right = helper1(root->right,level+1,val,depth);
+    return root;
+}
+TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+    if(depth==1){
+        TreeNode* node = new TreeNode(val);
+        node->left = root;
+        return node;
+    }
+    return helper1(root,1, val, depth);
+}
+
+//===思路2
+    
+TreeNode* helper(TreeNode* root, int level, int isleft, int val, int depth){
+//        if(!root) return NULL;
+    if(!root && (level==depth)){
+    }else if(!root){ return NULL; }
+       
+    if(level==depth && isleft){
+        TreeNode *tmp = new TreeNode(val);
+        tmp->left = root;//連接
+        return tmp;
+    }else if(level==depth && !isleft){
+        TreeNode *tmp = new TreeNode(val);
+        tmp->right = root;//連接
+        return tmp;
+    }
+
+    root->left = helper(root->left, level+1, 1, val, depth);
+    root->right = helper(root->right, level+1, 0,val, depth);        
+    return root;
+}
+//1<=depth<=depth of tree + 1 要考慮第一個,和最後一個     
+TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+    TreeNode* ret;
+    ret = helper(root, 1,1,val,depth);
+    return ret;
+}
+
+
+
+//=========
+class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int v, int d) {
         if(d==1){
