73da15a379dcca826b25fc3ddbf3d7708bf4742c
sammiiT <dirsing@gmail.com>
2023-02-08 01:29:02 +0800

    Update 429 N-ary Tree Level Order Traversal.cpp

--- Tree/429 N-ary Tree Level Order Traversal.cpp
+++ Tree/429 N-ary Tree Level Order Traversal.cpp
@@ -1,3 +1,24 @@
+//==類似題====
+428. Serialize and Deserialize N-ary Tree
+2039. The Time When the Network Becomes Idle
+//===思路===
+
+//=====
+void helper(Node* root, int level, vector<vector<int>>& res){
+    if(!root) return;
+    if(level==res.size()) res.push_back({});
+    res[level].push_back(root->val);
+
+    for(int i=0; i<root->children.size(); i++){
+        helper(root->children[i],level+1,res);
+    }
+}
+vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> res;
+        helper(root,0,res);
+        return res;   
+}
+//====
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
