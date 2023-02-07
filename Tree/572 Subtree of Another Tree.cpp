792b78c053056acfeb8454f36500eaca40cdac33
sammiiT <dirsing@gmail.com>
2023-02-07 22:06:17 +0800

    Update 572 Subtree of Another Tree.cpp

--- Tree/572 Subtree of Another Tree.cpp
+++ Tree/572 Subtree of Another Tree.cpp
@@ -1,3 +1,26 @@
+//===類似題===
+573. Squirrel Simulation
+250. Count Univalue Subtrees
+//===思路===
+(*)算法同isSameTree,但要以每一個節點做遍歷
+1. match函數就是 isSameTree()
+2.每次執行完一次match函數,就必須再以left,和right為根結點,繼續再執行一次match    
+3.isSubtree就是每次以child節點作為根節點, 再執行一次match
+//=======
+bool match(TreeNode* root, TreeNode* subRoot){
+    if(!root && !subRoot) return true;
+    if((!root&&subRoot) || (root&&!subRoot) || (root->val!=subRoot->val)) return false;
+    return match(root->left,subRoot->left) && match(root->right,subRoot->right);
+}
+
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;//沒有此項,會有access NULL pointer
+        if(match(root,subRoot)) return true;
+        return isSubtree(root->left, subRoot) || isSubtree(root->right,subRoot);      
+//        return isSubtree(root->left, subRoot->left) || isSubtree(root->right,subRoot->right);//比較錯誤,應該是比較原來subRoot
+    }
+//======
 class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
