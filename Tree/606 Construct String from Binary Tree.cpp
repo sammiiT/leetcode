643618ed0cc66ea8e6d3da7c2f8488c0ee35bab5
sammiiT <dirsing@gmail.com>
2023-02-07 23:03:38 +0800

    Update 606 Construct String from Binary Tree.cpp

--- Tree/606 Construct String from Binary Tree.cpp
+++ Tree/606 Construct String from Binary Tree.cpp
@@ -1,10 +1,13 @@
+//===類似題===
+609. Find Duplicate File in System
+536. Construct Binary Tree from String
+//===思路====
+(*)preorder (DFS)
+(*)如果遇到(!root->left && root->right),無左節點,有右節點 
+-需加入保留"()"
+//=====
 class Solution {
 public:
-    
-
-    
-    
-    
     string tree2str(TreeNode* t) {
         string s;
         if(!t) return s;
@@ -14,24 +17,17 @@ public:
         
         return s;
     }
-    
     void helper_2(TreeNode* root, string& s){
         if(!root) return;
-        
         s = s+"(";
-        
         s = s+to_string(root->val);
         if(!root->left && root->right){
             s = s + "()";
         }
         helper_2(root->left,s);
         helper_2(root->right,s);
-        
         s = s+")";
     }
-    
-    
-    
     string tree2str_ok(TreeNode* t) {
         string s;
         if(!t) return s;
@@ -47,10 +43,8 @@ public:
             if(r==1) s=s+"()";
             return;
         }
-        
         s=s+"(";
         s=s+to_string(root->val);
-        
        // helper(root->left,root->right?1:0,s);
         helper_ok(root->left,(root->right!=NULL),s);
         helper_ok(root->right,0,s);
