5be6e3179763ba124b6b1721b476874393e760e7
sammiiT <dirsing@gmail.com>
2023-02-09 00:29:22 +0800

    Update 783 Minimum Distance Between BST Nodes.cpp

--- Tree/783 Minimum Distance Between BST Nodes.cpp
+++ Tree/783 Minimum Distance Between BST Nodes.cpp
@@ -4,8 +4,28 @@
 2049. Count Nodes With the Highest Score
 2445. Number of Nodes With Value One
 //===思路===
-
+(*) 解法與 "530 Minimum Absolute Difference in BST" 一樣
 //=====
+void helper(TreeNode* root, int& pre, int& res){
+    if(!root) return;
+
+    helper(root->left,pre,res);
+    if(pre==-1) pre = root->val;
+    else{ 
+        res = min(res,abs(pre-root->val));
+        pre = root->val;
+    }
+    helper(root->right,pre,res);
+}
+
+int minDiffInBST(TreeNode* root) {
+        int pre = -1;
+        int res = INT_MAX;
+        helper(root,pre,res);
+        return res;
+}
+
+//====
 class Solution {
 public:
     int minDiffInBST(TreeNode* root) {
