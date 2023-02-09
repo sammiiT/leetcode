ac61a2e36b37b670f9807dcdc8b26f2fe757eb1d
sammiiT <dirsing@gmail.com>
2023-02-10 00:02:34 +0800

    Update 897 Increasing Order Search Tree.cpp

--- Tree/897 Increasing Order Search Tree.cpp
+++ Tree/897 Increasing Order Search Tree.cpp
@@ -1,4 +1,52 @@
-﻿class Solution {
+//===類似題===
+898. Bitwise ORs of Subarrays
+998. Maximum Binary Tree II
+549. Binary Tree Longest Consecutive Sequence II
+1717. Maximum Score From Removing Substrings
+
+//===思路===
+(*)post-order解法
+1.當下節點做排列
+   
+   rt        L  <---回傳L給上層
+  / \   =>    \    
+ L   R         rt
+                \
+                 R
+
+(*)需要用以下描述,不然 上層沒辦法連接下層更改的結果
+root->left = helper(root->left);
+root->right = helper(root->right);
+
+用此方式會上下層節點沒辦法做連接
+TreeNode* l = helper(root->left);
+TreeNode* r - helper(root->right);
+
+//=======
+TreeNode* helper(TreeNode* root){
+    if(!root) return NULL;
+    root->left = helper(root->left);
+    root->right = helper(root->right);
+    TreeNode *left = root->left;
+    TreeNode *right = root->right;
+
+    if(left){
+        root->left = NULL;
+        TreeNode* t=left;
+        while(t->right) t=t->right;
+        t->right = root;
+        return left;
+    }
+    return root;
+}
+TreeNode* increasingBST(TreeNode* root) {
+    return helper(root);
+}
+
+//===思路2===
+(*)inorder 概念求解
+//====
+class Solution {
 public:
     TreeNode* increasingBST(TreeNode* root) {
         TreeNode *res=NULL, *last=NULL;
