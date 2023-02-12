5561b3b787646f0ae3589647ea04a43af519a8df
sammiiT <dirsing@gmail.com>
2023-02-12 18:42:29 +0800

    Update 290. Word Pattern.cpp

--- hashmap_hashset/290. Word Pattern.cpp
+++ hashmap_hashset/290. Word Pattern.cpp
@@ -10,7 +10,8 @@ More challenges
 
 
 //===思路2===
-(*)用兩個unordered_map,來判斷pattern和string之間映射個數是否相同
+(*)用兩個unordered_map,來判斷pattern和string之間映射"順序"是否相同
+-如果用映射"個數"會錯誤
 //====
 bool wordPattern(string pattern, string str) {
   unordered_map<char,int> m1;
@@ -24,6 +25,17 @@ bool wordPattern(string pattern, string str) {
       if(i>=n || m1[pattern[i]]!=m2[word]) return false;
       m1[pattern[i]]=i+1;
       m2[word]=i+1;
+      
+/* //紀錄 "個數",會錯誤,因為沒有考慮到順序    
+      m1[pattern[i]]++;
+      m2[word]++;
+*/
   }
   return (i==n);
 }
+
+pattern="aba";
+s="dog cat cat";
+=>計算到第二個,m1[a]=1,m1[b]=1; m2[dog]=1,m2[cat]=1;
+=>當計算到第三個 m1[a]=1,m2[cat]=1;但m1[a]對應的必須是dog,而不是cat.
+
