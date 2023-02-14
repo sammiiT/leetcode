9838758a42ae0d09d1c227b2d78e27a424b6dc9f
sammiiT <dirsing@gmail.com>
2023-02-14 23:17:52 +0800

    Update 141 Linked List Cycle.cpp

--- LinkedList/141 Linked List Cycle.cpp
+++ LinkedList/141 Linked List Cycle.cpp
@@ -1,3 +1,26 @@
+//===類似題===
+142. Linked List Cycle II
+251. Flatten 2D Vector
+1813. Sentence Similarity III
+2488. Count Subarrays With Median K
+//===思路===
+
+//====
+bool hasCycle(ListNode *head) {
+    bool cycle=false;
+    ListNode *s1, *s2;
+    s1=s2=head;
+    while(s2&&s2->next){
+        s1=s1->next;
+        s2=s2->next->next;
+        if(s1==s2){
+            cycle=1;
+            break;
+        }
+    }
+    return cycle;
+}
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
