24d313b3bfcd19a5b0246d25fbd228f155f68e7b
sammiiT <dirsing@gmail.com>
2023-02-14 15:14:02 +0800

    Update 169 Majority Element.cpp

--- array/169 Majority Element.cpp
+++ array/169 Majority Element.cpp
@@ -1,3 +1,30 @@
+//===類似題====
+170. Two Sum III - Data structure design
+1150. Check If a Number Is Majority Element in a Sorted Array
+//===思路===
+1.建立一map, 記錄每一個數字的出現次數
+2.宣告一變數mxcnt,紀錄出現最多的次數
+3.兩次遍歷, 一次紀錄每一個數字出現次數, 一次判斷出現次數最多的數字
+//====
+int helper1(vector<int>& nums){
+    unordered_map<int,int> m;
+    int mxcnt = 0;
+    int ret = -1;
+    for(int a: nums){
+        m[a]++;
+        mxcnt = max(mxcnt,m[a]);
+    }
+    for(auto a:m){
+        if(a.second==mxcnt){
+            ret= a.first;
+            break;        
+        }
+    }
+    return ret;
+}
+//===思路2=====
+(*)賽馬算法
+//========
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
