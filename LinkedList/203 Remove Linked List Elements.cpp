3106aa287e578654e3c2a9af596b526129701a5a
sammiiT <dirsing@gmail.com>
2023-02-16 09:05:03 +0800

    Update 203 Remove Linked List Elements.cpp

--- LinkedList/203 Remove Linked List Elements.cpp
+++ LinkedList/203 Remove Linked List Elements.cpp
@@ -1,3 +1,25 @@
+//===類似題===
+204. Count Primes
+369. Plus One Linked List
+894. All Possible Full Binary Trees
+705. Design HashSet
+//===思路====
+
+//=====
+ListNode* removeElements(ListNode* head, int val) {
+    ListNode hdr(-1);
+    ListNode* cur;
+    hdr.next = head;
+    cur = &hdr;
+    while(cur->next){//如果下一個節點存在, 則繼續
+        if(cur->next->val==val)
+            cur->next = cur->next->next;
+        else cur=cur->next;
+    }
+    return hdr.next;
+}
+
+//===
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
