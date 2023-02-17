056caadff71f9684f1e2133ad2dbdbea7eb8c5c8
sammiiT <dirsing@gmail.com>
2023-02-17 11:06:35 +0800

    Update 485 Max Consecutive Ones.cpp

--- array/485 Max Consecutive Ones.cpp
+++ array/485 Max Consecutive Ones.cpp
@@ -1,3 +1,27 @@
+//===類似題===
+486. Predict the Winner
+487. Max Consecutive Ones II
+1869. Longer Contiguous Segments of Ones than Zeros
+2414. Length of the Longest Alphabetical Continuous Substring
+//===思路===
+1.遍歷數列, 遇到1就(++count), 並判斷maximum
+2.如果遇到0, 將count歸零
+3.最後回傳maximum 數值
+
+//====
+int helper0(vector<int>& nums) {
+    int res = 0;
+    int count = 0;
+    for(int a:nums){
+        if(a) res = max(res,++count);
+        else count = 0;
+    }
+    return res;
+}
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    return helper0(nums);
+}
+//===
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
