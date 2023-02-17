aa0743ad7a038688ab79be43dad797a141f343fb
sammiiT <dirsing@gmail.com>
2023-02-17 13:35:53 +0800

    Update 448 Find All Numbers Disappeared in an Array.cpp

--- array/448 Find All Numbers Disappeared in an Array.cpp
+++ array/448 Find All Numbers Disappeared in an Array.cpp
@@ -1,3 +1,28 @@
+//===類似題===
+449. Serialize and Deserialize BST
+1980. Find Unique Binary String
+2195. Append K Integers With Minimal Sum
+2295. Replace Elements in an Array
+//===思路===
+1.用unordered_map<int,int>記錄所有出現的數字
+2.從1~n遍歷, 如果1~n中的任何一個數沒有出現在map中
+3.如果沒有出現在map中, 將數值push_back至vecotr<int>中
+//====
+vector<int> helper0(vector<int>& nums){
+    unordered_map<int,int> m;
+    vector<int> res;
+    for(int a:nums) m[a]++;
+
+    for(int i=1; i<=nums.size(); i++){
+        if(!m.count(i)) res.push_back(i);
+    }    
+    return res;
+}
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    return helper0(nums);
+}
+
+//===思路2===
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums){
