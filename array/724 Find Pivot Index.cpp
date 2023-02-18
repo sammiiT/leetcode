0f46aaff18a8af384a150669d4a7fe3bac9057af
sammiiT <dirsing@gmail.com>
2023-02-19 01:03:29 +0800

    Update 724 Find Pivot Index.cpp

--- array/724 Find Pivot Index.cpp
+++ array/724 Find Pivot Index.cpp
@@ -1,4 +1,69 @@
-﻿class Solution {
+//===類似題===
+725. Split Linked List in Parts
+1991. Find the Middle Index in Array
+2270. Number of Ways to Split Array
+2219. Maximum Sum Score of Array
+//===思路===
+[2,1,-1]
+=>[2,3,2]
+=>[2,0,-1]
+
+
+[1,2,3]
+=>[1,3,6]
+=>[6,5,3]
+
+
+[1,7,3,6,5,6]
+=>[1,8,11,17,22,28]
+=>[28,27,20,17,11,6]
+
+//=====
+   int helper0(vector<int>& nums) {
+        vector<int> f;
+        vector<int> b;
+        int res = -1;
+        int sum =0;
+        for(int i=0;i<nums.size();i++){
+            sum +=nums[i];
+            f.push_back(sum);
+        }
+        sum = 0;
+        for(int i=nums.size()-1; i>=0; i--){
+            sum +=nums[i];
+            b.insert(b.begin(),sum);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(f[i]==b[i]){
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+int helper1(vector<int>& nums) {
+//    vector<int> f;
+    vector<int> b;
+    int res = -1;
+    int sum =0;
+
+    for(int i=nums.size()-1;i>=0;i--){
+        sum+=nums[i];
+        b.insert(b.begin(),sum);
+    }
+    sum = 0;
+    for(int i=0; i<nums.size(); i++){
+        sum+=nums[i];
+        if(sum==b[i]){
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+
+//===
+class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
