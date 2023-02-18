9caef3ba47051024b445595ef3e911c485584931
sammiiT <dirsing@gmail.com>
2023-02-18 22:22:09 +0800

    Update 605 Can Place Flowers.cpp

--- array/605 Can Place Flowers.cpp
+++ array/605 Can Place Flowers.cpp
@@ -1,3 +1,31 @@
+//===類似題===
+606. Construct String from Binary Tree
+495. Teemo Attacking
+//===思路===
+1.將數列前後分別加上一個"0" 
+flowerbed.insert(flowerbed.begin(),0); //前面加上一個0
+flowerbed.push_back(0);//最後加上一個0
+
+2.從index =1開始判斷 ,一直到 index<flowerbed.size()-1; (index = flowerbed.size()-2)
+3.此區間判斷不會超過陣列最大值
+4.如果第i個是"0", 則判斷 (i-1)和(i+1)是否等於"0" ;如果是0,則可以插入1
+5.以此判斷到 flowerbed.size()-2; 總共的1的數目和n比較,滿足題意則回傳true
+//===
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    int res = 0;
+    flowerbed.insert(flowerbed.begin(),0);
+    flowerbed.push_back(0);
+    for(int i=1;i<flowerbed.size()-1;i++){
+        if(flowerbed[i]==0){
+            if(flowerbed[i-1]==0 && flowerbed[i+1]==0) {
+                res++;
+                flowerbed[i]=1;
+            }        
+        }
+    }
+    return (res>=n);
+}
+//====
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
