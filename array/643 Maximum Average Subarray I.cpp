5efac5b86c1ba3aa5e9d3135aac55bbb12b9290b
sammiiT <dirsing@gmail.com>
2023-02-19 00:10:26 +0800

    Update 643 Maximum Average Subarray I.cpp

--- array/643 Maximum Average Subarray I.cpp
+++ array/643 Maximum Average Subarray I.cpp
@@ -1,3 +1,49 @@
+//===類似題===
+644. Maximum Average Subarray II
+644. Maximum Average Subarray II
+2090. K Radius Subarray Averages
+//===思路===
+1. sum 陣列, 每一個元素紀錄了index_0~index_i的總和
+2. 如果計算 i~j的總和; 則sum[j]-sum[i-1]
+//====
+double helper1(vector<int>& nums, int k){
+        vector<int> sum=nums;
+        double mx = INT_MIN;
+        for(int i=1; i<nums.size(); i++){
+            sum[i]=sum[i-1] + nums[i];
+        }
+        for(int i=k; i<nums.size(); i++){
+            mx=max((double)(sum[i]-sum[i-k])/k,mx);
+        }
+        mx = max((double)sum[k-1]/k, mx);
+        return mx;
+}
+
+double helper2(vector<int>& nums, int k){
+        vector<int> sum = nums;
+        double mx;
+
+        for(int i=1; i<nums.size(); i++){
+            sum[i]=sum[i-1]+nums[i];
+        }
+        mx = sum[k-1];
+        for(int i=k; i<nums.size(); i++){
+            mx = max((double)(sum[i]-sum[i-k]),mx);
+        }
+        return mx/k;
+}
+
+double helper0(vector<int>& nums, int k){//time limit exceeded
+        double mx = INT_MIN;
+        int n = nums.size();
+        for(int i=k; i<=n; i++){//log(n^2)
+            double sum = 0;
+            for(int j=i-k; j<i; j++) { sum+=nums[j]; }
+            mx = max(sum/k,mx);        
+        }
+        return mx;
+}
+//=====
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
