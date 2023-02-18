b02ba6df764a35303339d3ae212685bf7a5f419e
sammiiT <dirsing@gmail.com>
2023-02-18 08:53:42 +0800

    Update 589 N-ary Tree Preorder Traversal.cpp

--- Tree/589 N-ary Tree Preorder Traversal.cpp
+++ Tree/589 N-ary Tree Preorder Traversal.cpp
@@ -1,3 +1,13 @@
+//===類似題===
+590. N-ary Tree Postorder Traversal
+1361. Validate Binary Tree Nodes
+2313. Minimum Flips in Binary Tree to Get Result
+302. Smallest Rectangle Enclosing Black Pixels
+//===思路===
+(*)概念跟pre-order BST方法一致
+-BST是root->left, root->right
+-Node是 for(Node* a :root->children)//遍歷每一個子節點
+//====
 class Solution {
 public:
     vector<int> preorder(Node* root) {
