3a2661266f91ecac8b2eae82c3b56f619915ea8e
sammiiT <dirsing@gmail.com>
2023-02-20 14:31:30 +0800

    Update 905 Sort Array By Parity.cpp

--- array/905 Sort Array By Parity.cpp
+++ array/905 Sort Array By Parity.cpp
@@ -1,3 +1,12 @@
+//===類似題===
+906. Super Palindromes
+2164. Sort Even and Odd Indices Independently
+//===思路===
+1.遍歷數列
+-奇數用push_back放到陣列後方
+-偶數用insert放到陣列前方
+//===
+
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
