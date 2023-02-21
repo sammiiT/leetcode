2b01c6e79b8b03406f104ade263a191cc2d50b26
sammiiT <dirsing@gmail.com>
2023-02-21 15:32:36 +0800

    Update 1572. Matrix Diagonal Sum.cpp

--- array/1572. Matrix Diagonal Sum.cpp
+++ array/1572. Matrix Diagonal Sum.cpp
@@ -7,7 +7,6 @@
 1.遍歷所有位址
 2.(i,i)位址全部加起來
 3.(i,n-1-i)位址全部加起來
-
 //====
 int helper0(vector<vector<int>>& mat){
     int n = mat.size(), m = mat[0].size();
@@ -20,7 +19,19 @@ int helper0(vector<vector<int>>& mat){
     }
     return res;;
 }
-
 int diagonalSum(vector<vector<int>>& mat) {
     return helper0(mat);
+}
+//===思路2===
+(*)遍歷1次數列
+(*)如果m=n=奇數,則不重覆計算中心點
+//====
+int helper1(vector<vector<int>>& mat){
+    int m = mat.size();
+    int n = mat[0].size();
+    int res = 0;
+    for(int i=0;i<m;i++){
+       res = res + mat[i][i]+ ((n-i-1)==i?0:mat[i][n-i-1]);
+    }
+    return res;
 }
