9cdf73cfc6fd9761eb2612c4408405572c22edb8
sammiiT <dirsing@gmail.com>
2023-02-21 10:51:44 +0800

    Update 1200 Minimum Absolute Difference.cpp

--- array/1200 Minimum Absolute Difference.cpp
+++ array/1200 Minimum Absolute Difference.cpp
@@ -1,3 +1,27 @@
+//===類似題===
+1201. Ugly Number III
+2144. Minimum Cost of Buying Candies With Discount
+//===思路===
+(*)先sort陣列
+1.遍歷數列, 第一次計算minimum
+2.遍歷數列,第二次紀錄滿足minimum的兩元素
+//====
+vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+    int mi = INT_MAX;
+    vector<vector<int>> res;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<arr.size()-1;i++){
+        mi = min(mi,(arr[i+1]-arr[i]));
+    }
+
+    for(int i=0;i<arr.size()-1;i++){
+        if(mi == (arr[i+1]-arr[i]))
+            res.push_back({arr[i],arr[i+1]});
+    }
+    return res;
+}
+
+//===
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
