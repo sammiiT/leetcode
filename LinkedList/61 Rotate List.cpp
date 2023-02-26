57d2f02cbcd8525c234696f13595863824267e5c
sammiiT <dirsing@gmail.com>
2023-02-26 17:13:49 +0800

    Update 61 Rotate List.cpp

--- LinkedList/61 Rotate List.cpp
+++ LinkedList/61 Rotate List.cpp
@@ -1,4 +1,49 @@
-﻿class Solution {
+//===類似題===
+62. Unique Paths
+1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers
+1836. Remove Duplicates From an Unsorted Linked List
+1634. Add Two Polynomials Represented as Linked Lists
+//===思路===
+
+
+//=====
+ListNode* helper(ListNode* head, int k) {
+    ListNode hdr(-1);
+    ListNode *f,*r;
+    
+    if(!head||!head->next) return head;
+    hdr.next = head;
+    f=r=&hdr;
+/*計算節點個數*/    
+    int cnt = 0;
+    while(r&&r->next){
+        r=r->next;
+        cnt++;
+    }
+/*計算k是不是會超出cnt範圍*/    
+    k%=cnt;//k=k/cnt;
+    if(k==0) return head;
+    
+/*計算要被rotate節點的前一個節點, */
+    r = f;
+    for(int i=0;i<k;i++){ r=r->next; }
+    while(r&&r->next){
+        f=f->next;
+        r=r->next;
+    }
+/*執行rotate,區段重新段開再連結*/
+    ListNode* tmp = f->next;
+    f->next = r->next;
+    r->next = hdr.next;
+    hdr.next = tmp;
+
+    return hdr.next;
+
+}
+
+
+//===
+class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head==NULL||head->next==NULL) return head;
