355c26d15b3806173681b2a074c351487b35c7eb
sammiiT <dirsing@gmail.com>
2023-02-27 11:14:46 +0800

    Update 53 Maximum Subarray.cpp

--- DynamicProgramming/53 Maximum Subarray.cpp
+++ DynamicProgramming/53 Maximum Subarray.cpp
@@ -3,7 +3,15 @@
 978. Longest Turbulent Subarray
 2321. Maximum Score Of Spliced Array
 1749. Maximum Absolute Sum of Any Subarray
+//===思路1===
+1.遍歷陣列, 計算每一個節點的總和
+2.如果遇到總和是小於當下節點, 則以當下節點作為新的subarray頭, 開始做運算
+-- 用max(x,y)來做截斷的動作
+--每一次的運算都是是一個sum"總和",
+3.每一次的sum都必須再跟過去的maximum做比較, 過去的maximum就是之前的subarray總和
 
+4.回傳最後的maximum
+//=====
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -55,4 +63,23 @@ int helper2(vector<int>& nums){
             }
         }
         return res;
+}
+
+
+//===思路2===
+(*)用2維dynamic programming,會造成 memory exceeded limit
+(*)用1維的dynamic programing
+
+//===
+int helper4(vector<int>& nums){
+    int n = nums.size();
+    int res ;
+    vector<int> dp(n);
+    
+    res = dp[0] = nums[0];//以第0個為基準點做運算
+    for(int i=1; i<n; i++){
+        dp[i]=max(dp[i-1]+nums[i],nums[i]);
+        res = max(dp[i],res);
+    }
+    return res;
 }
