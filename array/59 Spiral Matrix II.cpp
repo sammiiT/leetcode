e1a8beef99c964e4f6e6242ad024e16fd012aa94
sammiiT <dirsing@gmail.com>
2023-03-02 11:21:29 +0800

    Update 59 Spiral Matrix II.cpp

--- array/59 Spiral Matrix II.cpp
+++ array/59 Spiral Matrix II.cpp
@@ -1,4 +1,67 @@
-﻿class Solution {
+//===類似題===
+60. Permutation Sequence
+885. Spiral Matrix III
+2326. Spiral Matrix IV
+
+//===思路===
+(*)54.Spiral Matrix 方法 ,修正
+1.建立一nxn matrix
+2.執行Sprial, 並根據路徑計算(i,j)
+3.將對應的數值帶入 matrix[i][j]=val;
+//====
+void helper1(int k, vector<vector<int>>& res){
+    int m = k, n = k;
+    int cnt = 0;
+    int rmin=0,rmax=m-1;
+    int cmin=0,cmax=n-1;
+    enum{LEFT,RIGHT,UP,DOWN};
+    int action = RIGHT;
+    int i = 0, j = 0;
+    int num = 1;
+
+    while(cnt<m*n){//2.執行Sprial, 並根據路徑計算(i,j)
+        res[i][j]=num++;//3.將對應的數值帶入 matrix[i][j]=val;
+        if(++cnt<m*n){
+            switch(action){
+                case(RIGHT):
+                    if(j+1<=cmax){ j++;
+                    }else{
+                        i++;
+                        rmin++;
+                        action = DOWN;
+                    }               
+                break;
+                case(LEFT):
+                    if(j-1>=cmin){ j--;
+                    }else{
+                        i--;
+                        rmax--;
+                        action = UP;
+                    }
+                break;
+                case(UP):
+                    if(i-1>=rmin){ i--;
+                    }else{
+                        j++;
+                        cmin++;
+                        action = RIGHT;
+                    }
+                break;
+                case(DOWN):
+                    if(i+1<=rmax){ i++;
+                    }else{
+                        j--;
+                        cmax--;
+                        action = LEFT;
+                    }
+                break;
+            }
+        }
+    }
+}
+
+//====
+class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         //vector<int>(n): 初始值為0
