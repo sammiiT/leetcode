ba4fb7fa86c224ed714f2dec28bd6a65f5593fcb
sammiiT <dirsing@gmail.com>
2023-03-03 14:09:24 +0800

    Update 82 Remove Duplicates from Sorted List II.cpp

--- LinkedList/82 Remove Duplicates from Sorted List II.cpp
+++ LinkedList/82 Remove Duplicates from Sorted List II.cpp
@@ -1,4 +1,63 @@
-﻿class Solution {
+//===類似題===
+83. Remove Duplicates from Sorted List
+More challenges
+1836. Remove Duplicates From an Unsorted Linked List
+//===思路===
+
+(*)如果有發生重複的節點
+
+ f   r  t              r->val==t->val  f   r     t->next
+hdr  O  O  O  O  O  x  =============> hdr  O  %  O  O  O  x 
+                                            \---/
+                                            r->next = t->next;
+        
+                                                 r  
+ f   r     t           r->val!=t->val  f         r->next 
+hdr  O  %  O  O  O  x  =============> hdr  %  %  O  O  O  x 
+                                        \-------/ 
+                                       f->next=r->next
+                                       r = f->next;
+    
+(*)如果節點數值不同
+
+ f   r  t              r->val!=t->val       f  r
+hdr  O  O  O  O  O  x  =============>  hdr  O  O  O  O  O  x 
+                                           f=r; r=t;
+
+(*)最後跳出迴圈時,必須再判斷一次是否有duplicate發生
+-有duplicate則刪除,再回傳
+-沒有duplicate,直接回傳
+//====
+ListNode* helper1(ListNode* head){
+    ListNode hdr(-1);
+    ListNode *f,*r;
+    bool duplicate = 0;
+    hdr.next = head;
+    f = &hdr;
+    r = hdr.next;
+    
+    while(r && r->next){
+        ListNode *t = r->next;
+        if(r->val!=t->val){
+            if(duplicate){//check duplicate
+                f->next = r->next;
+                r = f->next;
+                duplicate = 0;
+            }else{
+                f = r;
+                r = t;
+            }
+            continue;
+        }
+        duplicate = 1;
+        r->next = t->next;
+    }
+    if(duplicate) f->next = r->next;
+    return hdr.next;
+}
+
+//===思路2===
+class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL||head->next==NULL) return head;
