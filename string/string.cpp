cdd5dddb35fa5a9038041c387c4eb9454e9c915a
sammiiT <dirsing@gmail.com>
2023-03-04 14:42:12 +0800

    Update string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -3,3 +3,10 @@ string s = "1234,5";
 for(int i=0; i<s.size(); i++)
   if(s[i]=="1"){//要用雙引號
   } 
+
+
+//======
+str.insert(str.begin(),'A');//插入character
+                            str.insert(str.begin(),"hello");//不能插入一個string, 會有錯誤
+
+str.insert(0,"hello");//插入一個string
