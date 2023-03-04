f3615bb0477804a02ed48837f5aad24b81ee0cd5
sammiiT <dirsing@gmail.com>
2023-03-04 14:51:11 +0800

    Update string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -10,3 +10,5 @@ str.insert(str.begin(),'A');//插入character
                             str.insert(str.begin(),"hello");//不能插入一個string, 會有錯誤
 
 str.insert(0,"hello");//插入一個string
+
+str.erase(str.begin()+(str.size()-1));//移除最後一個character
