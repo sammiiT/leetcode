2b356bdcb6470ee10685d58504327c57a4e57346
sammiiT <dirsing@gmail.com>
2023-03-24 23:42:47 +0800

    Update 880. Decoded String at Index.cpp

--- string/880. Decoded String at Index.cpp
+++ string/880. Decoded String at Index.cpp
@@ -1,31 +1,46 @@
 
-string helper(string s, int k){
-    int cnt =0;
-    string tmp;
-    string res;
-    int j=0;
+//===類似題===
+881. Boats to Save People
+1574. Shortest Subarray to be Removed to Make Array Sorted
+85. Maximal Rectangle
+140. Word Break II
 
-    for(int i=0;i<s.size();i++){
-        if(s[i]>='0'&& s[i]<='9'){
-            cnt = cnt*10 + (s[i]-'0');
-            
-        }else{//character
-            if(tmp.size() && cnt>0){
-                j+=tmp.size()*cnt;
-                if((k-1)<j){
-                //do something....
-                   // cout<<tmp<<endl;
-                    int back_step = (j-1)-(k-1)+1;
-                    res=tmp[tmp.size()-back_step];
-                    break;        
-                }else{//(k-1)>=j
-                    tmp.clear();
-                    cnt = 0;
-                }
-            }
-            tmp+=s[i];
-            cout<<tmp<<endl;
-        }
+//===思路===
+1.遍歷string, 累加所有character的總數, 如;
+
+s="leet2code3"
+=> leetleetcode,leetleetcode,leetleetcode =>共36個, index=36  
+  
+2. 字串"從後到前遍歷"   
+-如果遇到數字,就將index/(s[i]-'0')  
+-如果遇到字元,就將index-=1;
+           
+          /--遇到數字3
+leet2code3
+=> 36/3 = 12
+=> leetleetcode,leetleetcode,leetleetcode / 3 = leetleetcode 有12個
+       
+         /---遇到字元
+leet2code 
+=> 12-1=11; 
+=> leetleetcode-1字元 => leetleetcod
+  
+3.當k對index(所有的字元數目)取餘數等於零, 代表k整除index, k就落在這個字元上
+k= k%index =0
+k=10, index =10,  取餘數會等於0  
+
+leetleetco
+          \---減去de兩個字元之後, 剩10個, 第10個為'o' 即為解
+
+//===
+string decodeAtIndex(string s, int k) {
+    long index =0;//記錄總character數
+    for(char a:s) index = isdigit(a)?index*(a-'0'): index+1;
+    
+    for(int i=s.size()-1; i>=0; --i){
+        k%=index;
+        if(k==0 && isalpha(s[i])) return string(1,s[i]);
+        index = isdigit(s[i])?index/(s[i]-'0'):index-1;
     }
-    return res;
+    return "";
 }
