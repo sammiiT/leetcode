13163fa8c4480d0d24c991f034e6bade5cc2d2ac
sammiiT <dirsing@gmail.com>
2023-03-26 22:33:18 +0800

    Create 969. Pancake Sorting.cpp

---
+++ array/969. Pancake Sorting.cpp
@@ -0,0 +1,33 @@
+//===類似題===
+970. Powerful Integers
+2234. Maximum Total Beauty of the Gardens
+1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits
+1738. Find Kth Largest XOR Coordinate Value
+
+//===思路===
+1. 遍歷數列,找到最大的數值
+2. [0:最大數值的位置] 做reverse => (arr.begin(),arr.begin()+(i+1))
+3. [0:n] 做reverse  =>  (arr.begin(),arr.begin()+n)
+4. n--;//下次最大的數值
+
+(*)如果遇到最大的值在每次新update的最後位置, 則不用做reverse
+if(i==(n-1)) continue;
+//=========
+vector<int> pancakeSort(vector<int>& arr) {
+    vector<int> res;
+    int n = arr.size();
+    while(n){
+        int i;
+        
+        for(i=0; i<n; ++i)  if(arr[i]==n) break;
+        if(i==(n-1)) { --n; continue;}
+        
+        res.push_back(i+1);
+        reverse(arr.begin(),arr.begin()+(i+1));
+        
+        res.push_back(n);
+        reverse(arr.begin(),arr.begin()+n);
+        --n;
+    }
+    return res;
+}
