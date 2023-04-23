465aadbc2b01820ac23aafd1ef3c1cdbf3b9988c
sammiiT <dirsing@gmail.com>
2023-04-23 10:23:28 +0800

    Update 90. Subsets II.cpp

--- DFS/90. Subsets II.cpp
+++ DFS/90. Subsets II.cpp
@@ -5,12 +5,18 @@
 (*)DFS算法
 (*)先作排列sort,因為會有重複解發生
 1.每次進入下一個level, 都要將當下"解"vector<int>, push到最後的result中vector<vector<int>>
-2.為了防止重複解, 必須加上 if(i>start && nums[i]==nums[i-1]) continue;
+2.為了防止重複解, 必須加上 if(i>start && nums[i]==nums[i-1]) continue; ==> 同一層的判斷 
 Ex:[1,2,2]
 1為root,  [1,2],[1,2,2],和[1,2] (第三個2), 所以要把第三個2排除;所以加上上述判斷
 
 3.如果if(i>0 && nums[i]==nums[i-1]) 會錯誤, 會將[1,2,2]也排除
 
+root = 1, 配對child為 [2,2] ==> 位置2(0-index)的2可以是第二層,也可以是第三層; 在第二層時, 跟位置1(0-index)的2重複
+     1                     
+    / \ 
+   2   2(*)
+  /
+ 2(*)   
 //====
 class Solution {
 public:
@@ -36,7 +42,7 @@ public:
                vector<vector<int>>& res){
         res.push_back(out);
         for(int i=start; i<nums.size(); ++i){
-            if(i>start && nums[i]==nums[i-1]) continue;
+            if(i>start && nums[i]==nums[i-1]) continue;//同一層判斷
             out.push_back(nums[i]);
             helper(nums,i+1,out,res);
             out.pop_back();
