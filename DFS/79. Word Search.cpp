2338a6a911b5a0981b73a0ed4bb39731b761123f
sammiiT <dirsing@gmail.com>
2023-04-30 10:27:33 +0800

    Update 79. Word Search.cpp

--- DFS/79. Word Search.cpp
+++ DFS/79. Word Search.cpp
@@ -68,45 +68,18 @@ bool exist(vector<vector<char>>& board, string word) {
    return false; 
 }
 
-//====
+//===思路2==
+(*)優化,判斷每一個[x][y]的character是不是滿足 word[idx]的順序
+(*)每進入下一個level就判斷idx是否等於word.size()
+- 如果idx等於word.dize(),代表上一層的[x][y]排列滿足word
+
+(*)每往下一層, 都會有"上","下","左","右"的路徑可以走
+(*)vector<vector<bool>> visited來代表 已經走過的路徑
+
+
 class Solution {
 public:
-// bool exist(vector<vector<char>>& board, string word) {
-//         if (board.empty() || board[0].empty()) return false;
-//         int row = board.size(), col = board[0].size();
-//         vector<vector<bool>> visited(row, vector<bool>(col, false));
-//         for (int i = 0; i < row; ++i) {
-//             for (int j = 0; j < col; ++j) {
-//                 if (search(board, word, 0, i, j, visited)) return true;
-//             }
-//         }
-//         return false;
-//     }
-//     bool search(vector<vector<char>>& board, string word, int idx, int i, int j, vector<vector<bool>>& visited) {
-//         if (idx == word.size()) return true;
-//         int m = board.size(), n = board[0].size();
-//         if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || board[i][j] != word[idx]) return false;
-//         visited[i][j] = true;
-//         bool res = search(board, word, idx + 1, i - 1, j, visited) 
-//                  || search(board, word, idx + 1, i + 1, j, visited)
-//                  || search(board, word, idx + 1, i, j - 1, visited)
-//                  || search(board, word, idx + 1, i, j + 1, visited);
-//         visited[i][j] = false;
-//         return res;
-//     }    
-    
-    bool exist(vector<vector<char>>& board, string word) {
-        // int row,col;
-        // row = board.size(); col = board[0].size();
-        // if(row<=0 || col<=0) return false;
-        // vector<vector<bool>> visited(row,vector<bool>(col,false));
-        // for(int i = 0; i<row; ++i){//從任意一點開始,用i,j loop
-        //     for(int j = 0; j<col; ++j){
-        //         if(search(board,word,0,i,j,visited)) return true;   
-        //     }        
-        // }
-        // return false;
-        
+    bool exist(vector<vector<char>>& board, string word) {        
         if (board.empty() || board[0].empty()) return false;
         int row = board.size(), col = board[0].size();
         vector<vector<bool>> visited(row, vector<bool>(col, false));
@@ -117,7 +90,6 @@ public:
         }
         return false;
     }
-
 //問題問ˇ共有幾種走法 or 有沒有存在路徑? ===>屬於有沒有存在路徑問題
 //有些題目用for 迴圈, 此題不用, 試分析差異  //subsets 
     bool search(vector<vector<char>> &board,
@@ -128,7 +100,6 @@ public:
                 vector<vector<bool>> &visited){
         
         if(idx==word.size()) return true;
-        int row = board.size(),col=board[0].size();
         if(i<0||j<0||i>row-1||j>col-1   //(i,j)不在board範圍之內
            ||visited[i][j]          //重複讀取
            ||board[i][j]!=word[idx])//獨取到的數值不滿足word的條件 
