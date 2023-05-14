466a16bfb60ec60bf4d5b43fbfe7cfddf72dcf5b
sammiiT <dirsing@gmail.com>
2023-05-14 15:16:45 +0800

    Create 130. Surrounded Regions.cpp

---
+++ BFS/130. Surrounded Regions.cpp
@@ -0,0 +1,86 @@
+//===類似題===
+
+//===思路===
+(*)BFS
+
+//====
+void solve(vector<vector<char>>& board){  
+    int m = board.size(),n=board[0].size();
+    vector<vector<int>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+    queue<vector<int>> q;
+    for(int i=0; i<m; ++i){
+        for(int j=0; j<n; ++j){
+            if((i==0||i==m-1||j==0||j==n-1)&&board[i][j]=='O'){
+                q.push({i,j});                
+                board[i][j]='#';
+                while(!q.empty()){
+                    vector<int> pos=q.front();q.pop();
+                    for(auto dir:dirs){
+                        int x=pos[0]+dir[0], y=pos[1]+dir[1];
+                        if(x<0||x>=m||y<0||y>=n||board[x][y]!='O') continue;
+                        board[x][y]='#'; 
+                        q.push({x,y});
+                    }
+                }
+            } 
+        }
+    }
+    for(int i=0; i<m; ++i){
+        for(int j=0; j<n; ++j){
+            if(board[i][j]=='O') board[i][j]='X';
+            if(board[i][j]=='#') board[i][j]='O';
+        }
+    }
+}
+//=======================================
+void solve(vector<vector<char>>& board){  
+    int m = board.size(),n=board[0].size();
+    vector<vector<int>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+    queue<vector<int>> q;
+    for(int i=0; i<m; ++i){
+        for(int j=0; j<n; ++j){
+            if((i==0||i==m-1||j==0||j==n-1)&&board[i][j]=='O'){
+                q.push({i,j});                
+                board[i][j]='#';
+                while(!q.empty()){
+                    vector<int> pos=q.front();q.pop();
+                    for(auto dir:dirs){
+                        int x=pos[0]+dir[0], y=pos[1]+dir[1];
+                        if(x>0 && x<(m-1) && y>0 && y<(n-1) && board[x][y]=='O') {
+                            board[x][y]='#'; q.push({x,y}); 
+                        }
+                    }
+                }
+            } 
+        }
+    }
+    for(int i=0; i<m; ++i){
+        for(int j=0; j<n; ++j){
+            if(board[i][j]=='O') board[i][j]='X';
+            if(board[i][j]=='#') board[i][j]='O';
+        }
+    }
+}
+//==== Time limited Exceeded===
+(*)原因: 沒有先將連接邊界的'O'位置, 將其先改'#',
+ 所以會將座標值為'O'的, 重複加入queue中, 導致queue越來越大, 最後導致time limited eceeded
+
+
+    for(int i=0; i<m; ++i){
+        for(int j=0; j<n; ++j){
+            if((i==0||i==m-1||j==0||j==n-1)&&board[i][j]=='O'){
+                q.push({i,j});         
+                while(!q.empty()){
+                    vector<int> pos=q.front();q.pop();
+                    board[pos[0]][pos[1]]='#';
+                    for(auto dir:dirs){
+                        int x=pos[0]+dir[0], y=pos[1]+dir[1];
+                        if(x>0 && x<(m-1) && y>0 && y<(n-1) && board[x][y]=='O') { q.push({x,y});}
+                        //沒有先將連接邊界的'O'位置, 將其先改'#'
+                        //所以會將座標值為'O'的, 重複加入queue中, 導致queue越來越大, 最後導致time limited eceeded
+                    }
+                }
+            } 
+        }
+    }
+
