b59fbe5747d720ed6fdef09c5e33b106db41fa2b
sammiiT <dirsing@gmail.com>
2023-05-21 23:41:00 +0800

    Update 450 Delete Node in a BST.cpp

--- Tree/450 Delete Node in a BST.cpp
+++ Tree/450 Delete Node in a BST.cpp
@@ -12,10 +12,11 @@
 -如果right存在,就遍歷其left節點
 -right的最後一個left節點連接原來的left節點
 -回傳right節點位址給上層
+
+(*)post-order遍歷算法,會遍歷所有節點,所以會比較耗時
 //=====
 TreeNode* deleteNode(TreeNode* root, int key) {
     if(!root) return root;
-
     root->left = deleteNode(root->left, key);
     root->right = deleteNode(root->right, key);
 //...    
@@ -31,33 +32,66 @@ TreeNode* deleteNode(TreeNode* root, int key) {
     }
     return root;        
 }
+TreeNode* deleteNode(TreeNode* root, int key) {
+    if(!root) return root;
+    root->left = deleteNode(root->left, key);
+    root->right = deleteNode(root->right, key);
+    if(root->val==key){
+        TreeNode* r = root->right;
+        while(r&&r->left) r=r->left;
+        if(r){
+            r->left = root->left;
+            return root->right;
+        }else{
+            return root->left;
+        }
+    }
+    return root;
+}
+
+
 //===思路2===
 (*) pre-order算法
+(*)pre-order算法會比較節省時間, 因為不必像post-order一樣遍歷所有節點
+-pre-order遇到刪除的節點, 就直接運算, 不會遍歷到刪除節點以下的節點
 //===
 //用return的方式回傳下一個節點, 可以知道指定節點的前一個節點
 TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root) return NULL;
-    
-        if(root->val==key){//移除節點, 重新建構
-            TreeNode *l=root->left;
-            TreeNode *r=root->right;
-            TreeNode *tmp;
-            tmp = r;
-            while(tmp&&tmp->left){
-                tmp=tmp->left;
-            }
-            if(tmp){
-                tmp->left = l;
-                return r;
-            }else{
-                return l;
-            }
+    if(!root) return NULL;
+    if(root->val==key){//移除節點, 重新建構
+        TreeNode *l=root->left;
+        TreeNode *r=root->right;
+        TreeNode *tmp;
+        tmp = r;
+        while(tmp&&tmp->left){
+            tmp=tmp->left;
         }
-        //用return的方式回傳下一個節點, 可以知道指定節點的前一個節點
-        if(root->left) root->left = deleteNode(root->left, key);
-        if(root->right) root->right = deleteNode(root->right, key);
-        
-        return root;    
+        if(tmp){
+            tmp->left = l;
+            return r;
+        }else{
+            return l;
+        }
+    }
+    //用return的方式回傳下一個節點, 可以知道指定節點的前一個節點
+    if(root->left) root->left = deleteNode(root->left, key);
+    if(root->right) root->right = deleteNode(root->right, key);
+    return root;    
+}
+
+TreeNode* deleteNode(TreeNode* root, int key) {
+    if(!root) return　NULL;
+    if(root->val==key){
+        TreeNode* r = root->right;
+        while(r&&r->left) r = r->left;//如果有right節點, 遍歷right節點的left
+        if(r){ 
+            r->left = root->left;//連接right的left-leaf 和root->left
+            return root->right;  //回傳root->right給上層
+        } else return root->left;//如果沒有right節點, left節點直接回傳
+    }
+    root->left = deleteNode(root->left,key);
+    root->right = deleteNode(root->right,key);
+    return root;
 }
 
 //=====
@@ -120,12 +154,6 @@ public:
         if(root && root->right) return maxNode(root->right);
         else return root;
     }
-    
-    
-    
-    
-    
-    
     
     //橫跨3個節點, 用中間的節點來作運算,比較容易
     TreeNode* deleteNode_ok(TreeNode* root, int key) {
