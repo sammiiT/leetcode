c2883ae83a57f2106ddba9f57b2889a8f66b2589
sammiiT <dirsing@gmail.com>
2023-05-22 22:42:49 +0800

    Update 508 Most Frequent Subtree Sum.cpp

--- Tree/508 Most Frequent Subtree Sum.cpp
+++ Tree/508 Most Frequent Subtree Sum.cpp
@@ -2,7 +2,7 @@
 510. Inorder Successor in BST II
 1973. Count Nodes Equal to Sum of Descendants
 //===思路===
-(*)跟mode那一題很像,只是這一題是算subtree sum
+(*)跟mode那一題很像,只是這一題是算subtree_sum
 (*)post-order (DFS)
 -計算完下層節點之後, 回傳下層節點的總和,所以會有left的總和,right的總和	
 -依照題意,subtree_sum, 所以 當下的節點要加上left和right;得到的總和就是subtree_sum
@@ -29,11 +29,8 @@ public:
                 }
             }
         }
-        return res;
-        
+        return res;        
     }
-    
-    
     
     int helper(TreeNode* root, 
 		int& cnt,//紀錄 出現最多的次數 
@@ -45,10 +42,25 @@ public:
         if(root->right) r = helper(root->right, cnt, m);
         
         int total = root->val + l + r;
-        m[total]++;
-        
+        m[total]++;        
         cnt = max(m[total],cnt);
-        
         return total;
     }
 };
+//=====
+int helper(TreeNode* root, int& mx ,unordered_map<int,int>& mp){
+    if(!root) return 0;
+    int ret = root->val + helper(root->left, mp)+helper(root->right,mp);    
+    mx = max(mx,++mp[ret]);
+    return ret;
+}
+vector<int> findFrequentTreeSum(TreeNode* root) {
+    unordere_map<int,int> mp;//value,count
+    vector<int> res;
+    int mx = 0;
+    helper(root,mx,mp);    
+    for(auto a:mp){ if(mx==a.second) res.push_back(a.first); }
+    return res;
+}
+
+
