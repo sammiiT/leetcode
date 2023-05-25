c377fcd279abfe0c5662e696e4b622d124b2d06d
sammiiT <dirsing@gmail.com>
2023-05-26 01:01:38 +0800

    Update 287 Find the Duplicate Number.cpp

--- array/287 Find the Duplicate Number.cpp
+++ array/287 Find the Duplicate Number.cpp
@@ -1,3 +1,22 @@
+//===類似題===
+609. Find Duplicate File in System
+645. Set Mismatch
+
+//===思路====
+(*)固定size陣列; vector<int> m(10001,0)
+ -  1<=n<=10^5,所以定義 如上vector, 不管如何都constant size的 vector   
+
+int findDuplicate(vector<int>& nums){
+    vector<int> m(10001,0);
+    for(int a:nums){
+        if(++m[a]>=2) return a;
+    }
+    return -1;
+}
+
+
+//===思路2===
+(*)用unordered_map做
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
