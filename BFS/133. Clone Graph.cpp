fcec3c7878933ae8d6186de9c812ac27d585b037
sammiiT <dirsing@gmail.com>
2023-05-30 00:12:45 +0800

    Create 133. Clone Graph.cpp

---
+++ BFS/133. Clone Graph.cpp
@@ -0,0 +1,38 @@
+//===類似題==
+1490. Clone N-ary Tree
+1485. Clone Binary Tree With Random Pointer
+1490. Clone N-ary Tree
+
+//===思路===
+(*)BFS+DFS
+-BFS部分, 是for(Node* n:node->neighbors)
+-DFS部分, 是 每次進入clone->neighbors.push_back(helper(n,mp)),並不會將同一層for loop執行完; 而是一直進入下一層
+   A-------B                 
+   |       |
+   |       |
+   Y-------X
+  
+-建立新節點的順序, A-->B-->X--->Y--->'A'
+-A已經在map中,且有對應的clone, 所以直接回傳給Y
+-接著Y那一層, 會接著計算剩下相鄰的node(除了A之外的節點);對應的節點是X
+-X也已經在map中建立,有對應的clone, 所以會直接回傳給Y
+-Y的BFS計算完成,跳回上一層X;計算X剩下的(BFS)相鄰節點
+-接下來的節點以此類推
+
+(*)用map紀錄 clone值,每一個舊的NODE都會對應一個新的NODE
+
+//====
+Node* helper(Node* node, unordered_map<Node*,Node*>& mp){
+  if(!node) return NULL;
+  if(mp.count(node)) return mp[node]; 
+  
+  Node* clone = new Node(node->val);
+  mp[node]=clone;
+  for(Node* n:node->neighbors){
+    clone->neighbors.push_back(helper(n,mp));
+  }
+  return clone;
+}
+
+Node* cloneGraph(Node* node) {
+}
