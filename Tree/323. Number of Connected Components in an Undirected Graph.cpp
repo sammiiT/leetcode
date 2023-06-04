8e294ab101dc048d64cef23d472a3678b91daa91
sammiiT <dirsing@gmail.com>
2023-06-04 17:17:51 +0800

    Update 323. Number of Connected Components in an Undirected Graph.cpp

--- Tree/323. Number of Connected Components in an Undirected Graph.cpp
+++ Tree/323. Number of Connected Components in an Undirected Graph.cpp
@@ -14,7 +14,6 @@ vector<vector<int>> g(n);//每一個節點的neighbor
 (*)判斷節點是否拜訪過
 vector<bool> visited
 //=====
-
 void dfs(vector<vector<int>>& g, vector<bool>& visited, int i){
   if(visited[i]) return;
   visited[i]=true;
@@ -38,3 +37,39 @@ int countComponents(int n, vector<pair<int, int> >& edges) {
   }
   return res;
 }
+//===思路2===
+(*)從0開始, 0-index
+- 建立vector<bool> visited, 判斷節點是否被遍歷過; index = 節點
+- 建立unordered_map<int,vector<int>> mp, 紀錄每一個節點的相鄰節點; key = 節點, value = 相鄰節點  
+- 每次遍歷完所有的graph,就將result累加一次
+
+
+void dfs(int i,//節點 
+         vector<bool>& visited,//判斷節點是否遍歷過 
+         unordered_map<int,vector<int>>& mp){//每一個節點的相鄰節點
+    if(visited[i]) return;
+    visited[i]=true;
+    for(int p: mp[i]){
+        dfs(p,visited,mp);
+    }
+}
+
+int countComponents(int n, vector<pair<int, int> >& edges) {
+    int res = 0;
+    
+    vector<bool> visited(n,false); 
+    unordered_map<int,vector<int>> mp;
+    
+    for(auto p:edges){
+        mp[p.first].push_back(p.second);
+        mp[p.second].push_back(p.first);
+    }
+    
+    for(int i=0; i<n; ++i){
+         if(visited[i]) continue;//節點是否有遍歷過,是否存在於一個connection中
+         dfs(i,visited,mp);
+         res++;
+    }
+    return res;
+}
+
