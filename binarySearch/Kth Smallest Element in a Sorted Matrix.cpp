de3179d8f7c48b9e07f0d3e13956ffc777b67914
sammiiT <dirsing@gmail.com>
2023-06-13 22:59:59 +0800

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- binarySearch/Kth Smallest Element in a Sorted Matrix.cpp
+++ binarySearch/Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,3 +1,15 @@
+//===類似題===
+230. Kth Smallest Element in a BST
+668. Kth Smallest Number in Multiplication Table
+719. Find K-th Smallest Pair Distance
+786. K-th Smallest Prime Fraction
+
+//===思路===
+(*)概念: 0-index
+arr[i] => index i前面有i個element
+uppder_bound回傳index表示前面有 index個element
+
+//====
 int kthSmallest(vector<vector<int>>& matrix, int k){
      int l = matrix[0][0],r = matrix.back().back();
      while(l<r){
