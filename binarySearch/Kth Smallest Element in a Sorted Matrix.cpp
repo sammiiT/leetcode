78c0cdd7a0dd87139323e9c4b4543472561ceed1
sammiiT <dirsing@gmail.com>
2023-06-17 22:31:07 +0800

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- binarySearch/Kth Smallest Element in a Sorted Matrix.cpp
+++ binarySearch/Kth Smallest Element in a Sorted Matrix.cpp
@@ -25,5 +25,14 @@ int kthSmallest(vector<vector<int>>& matrix, int k){
     }
     return r;
 }
-
-
+//===思路2===
+int kthSmallest(vector<vector<int>>& matrix, int k){
+     priority_queue<int> pq;
+     for(int i=0; i<matrix.size(); ++i){
+          for(int j=0; j<matrix[i].size(); ++j){
+               pq.push(matrix[i][j]);
+               if(pq.size()>k) pq.pop();
+          }
+     }
+     return pq.top();
+}
