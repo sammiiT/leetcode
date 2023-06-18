3244ea8ec0f1d1758d73550eeac55968ce9f2b53
sammiiT <dirsing@gmail.com>
2023-06-18 10:41:15 +0800

    Update 611 Valid Triangle Numbeer.cpp

--- array/611 Valid Triangle Numbeer.cpp
+++ array/611 Valid Triangle Numbeer.cpp
@@ -1,4 +1,41 @@
-﻿class Solution {
+//===類似題===
+1. Two Sum
+259. 3Sum Smaller
+//===思路===
+(*) 兩邊和大於第三邊
+(*) i>=2: index=2時, 左邊有2個元素
+(*)從最大邊開始計算
+
+	[l]           [r=i-1]  [i]
+	a   b  c  d    e        f      [l] + [r] > [i]  => 可以有 ae, be, ce, de  共4種
+
+	[l]       [r=r--]      [i]     [l] + [r] > [i]  => 可以有 ad, bd, cd   共三種
+	a   b  c   d     e      f  
+
+
+[l]           [r=i-1]  [i]
+ a   b  c  d    e        f      [l] + [r] <= [i]  => l++往右邊移動, 往邊長增加的方向移動
+
+//==
+int triangleNumber(vector<int>& nums) {
+	int res = 0;
+	sort(nums.begin(),nums.end());
+	for(int i=nums.size()-1; i>=2; ++i){
+		int l = 0,r = i-1;
+		while(l<r){
+			if(nums[l]+nums[r]>nums[i]){
+				res += (r-l);//不包含r,r本身
+				--r;
+			}else{
+				++l;
+			}
+		}
+	}
+	return res;
+}
+
+//===思路2===
+class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         int res = 0, n = nums.size();
