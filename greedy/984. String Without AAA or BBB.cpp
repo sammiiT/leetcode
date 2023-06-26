0c6af2cd3c5e192dd1a189a49e75b2dd43465db1
sammiiT <dirsing@gmail.com>
2023-06-26 23:56:32 +0800

    Update 984. String Without AAA or BBB.cpp

--- greedy/984. String Without AAA or BBB.cpp
+++ greedy/984. String Without AAA or BBB.cpp
@@ -44,5 +44,39 @@ string strWithout3a3b(int a, int b) {
 }
 
 //====思路====
+(*)利用priority_queue 求解
+
+string helper2(int a, int b) {
+    priority_queue<pair<char,int>,vector<pair<char,int>>,cmp> pq;
+    string res;
+    int cntx=0,cnty=0;
+    pq.push({'a',a});
+    pq.push({'b',b});
+    
+    while(pq.size()>=2){
+        pair<char,int> x = pq.top();pq.pop();
+        pair<char,int> y = pq.top();pq.pop();
+        
+        if(x.second-y.second>=1){
+            cntx = 2;
+            cnty = 1;
+        }else if(x.second-y.second==0){
+            cntx = (x.second>=2)?2:1;
+            cnty = cntx;
+        }
+        res = res + string(cntx,x.first);
+        res = res + string(cnty,y.first);
+        x.second-=cntx;
+        y.second-=cnty;
+        if(x.second>0) pq.push({x.first,x.second});
+        if(y.second>0) pq.push({y.first,y.second});
+    }
+    if(pq.size()==1){
+        pair<char,int> t = pq.top();pq.pop();
+        cntx = t.second>=2?2:1;
+        res = res +string(cntx,t.first);
+    }
+    return res;
+}
 
 
