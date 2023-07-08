ffb09af2dfa30027deb264c01a83ff8d24809251
sammiiT <dirsing@gmail.com>
2023-07-08 11:52:34 +0800

    Update 150. Evaluate Reverse Polish Notation.cpp

--- stack/150. Evaluate Reverse Polish Notation.cpp
+++ stack/150. Evaluate Reverse Polish Notation.cpp
@@ -55,3 +55,33 @@ int evalRPN(vector<string>& tokens) {
     }
     return stk.top();     
 }
+
+//====寫法2===
+int evalRPN(vector<string>& tokens){
+    stack<int> stk;
+    for(string str:tokens){
+        if(str=="+"){
+            int a = stk.top(); stk.pop();
+            int b = stk.top(); stk.pop();
+            stk.push(b+a);
+        }else if(str=="-"){
+            int a = stk.top(); stk.pop();
+            int b = stk.top(); stk.pop();
+            stk.push(b-a);
+            
+        }else if(str=="*"){
+            int a = stk.top(); stk.pop();
+            int b = stk.top(); stk.pop();
+            stk.push(b*a);
+        
+        }else if(str=="/"){
+            int a = stk.top(); stk.pop();
+            int b = stk.top(); stk.pop();
+            stk.push(b/a);
+            
+        }else{
+            stk.push(stoi(str));
+        }
+    }
+    return stk.top();
+}
