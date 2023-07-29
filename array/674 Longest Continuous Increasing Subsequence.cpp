995db9cdca89b53070ff5dc184e4741930a7952e
sammiiT <dirsing@gmail.com>
2023-07-29 21:11:25 +0800

    Update 674 Longest Continuous Increasing Subsequence.cpp

--- array/674 Longest Continuous Increasing Subsequence.cpp
+++ array/674 Longest Continuous Increasing Subsequence.cpp
@@ -1,3 +1,28 @@
+//===類似題===
+300. Longest Increasing Subsequence
+673. Number of Longest Increasing Subsequence
+727. Minimum Window Subsequence
+2407. Longest Increasing Subsequence II
+//===思路===
+(*)若[i-1]<[i] 則 continuous subsequence 累加
+(*)若[i-1]>=[i], 則continuous subsequence 重製
+
+int findLengthOfLCIS(vector<int>& nums) {
+        int res = 1;
+        int cur = 1;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i-1]<nums[i]){
+                cur+=1;
+            }else{
+                cur=1;
+            }
+            res = max(res,cur);
+        }
+        return res;
+}
+
+
+//===寫法2===
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
