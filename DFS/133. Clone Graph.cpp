568260e4c0fa968eba2f9386d10f2582cdd65204
sammiiT <dirsing@gmail.com>
2023-08-14 00:10:09 +0800

    Update 133. Clone Graph.cpp

--- DFS/133. Clone Graph.cpp
+++ DFS/133. Clone Graph.cpp
@@ -21,3 +21,18 @@ Node* cloneGraph(Node* node) {
   unordered_map<Node*,Node*> mp;//curNode, newNode
   return helper(node,mp);
 }
+
+//==== 輔助函式helper 改寫 ===
+
+Node* helper(Node* node, unordered_map<Node*,Node*>& ump){
+    if(!node) return NULL;
+    if(ump.count(node)){
+        return ump[node];
+    }
+    Node* clone = new Node(node->val);
+    ump[node]=clone;
+    for(int i=0; i<node->neighbor.size(); ++i){
+        clone->neighbors.push_back(helper(node->neighbor[i],ump));
+    }
+    return clone;
+}
