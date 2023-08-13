21c90ee0236a05902849f00d1e49c22e7c2dc4a2
sammiiT <dirsing@gmail.com>
2023-08-13 09:36:16 +0800

    Update 290. Word Pattern.cpp

--- string/290. Word Pattern.cpp
+++ string/290. Word Pattern.cpp
@@ -4,9 +4,47 @@ More challenges
 291. Word Pattern II
 //===思路1====
 (*) 用unordered_map建立pattern和string之間的映射
+(*) unordered_map<char,string>; character對應string的map
+unordered_map<string,char>; string對應character的 map
+
+(*)istringstream和ostringstream使用
+  
+(*)以pattern的char為依據做遍歷.
+1.先建立 string的 substring pattern 到 vector<string>中; 再與pattern string做比較
+2.遍歷 pattern來判斷對應的 string
+
+(@1)狀況一
+  a      b      a
+   \      \      \
+    dog    cat    fish   
+
+(@2)狀況二
+  a      b      c
+   \      \      \
+    dog    cat    dog   
+
 
 //====
-
+bool wordPattern(string pattern, string str) {
+  vector<string> words;
+  istringstream in(str);
+  unordered_map<char,string> mpcs;
+  unordered_map<string,char> mpsc;
+  
+  for(string word;in>>word;){ words.push_back(word);}
+  if(pattern.size()!=words.size()) return false;//"aaa" ; "aa aa aa aa"
+  
+  for(int i=0; i<pattern.size(); ++i){
+    if(mpcs.count(pattern[i])) {
+      if(mpcs[pattern[i]]!=words[i]) return false;//如果原來有對應string的char, 對應了新的string  (@1)
+    } else {//!mpcs.count(pattern[i])
+      if(mpsc.count(words[i])) return false;// 新的char對應的string不存在;但string在之前有對應的char (@2)
+      mpcs[pattern[i]]=words[i];
+      mpsc[words[i]]=pattern[i];
+    } 
+  }
+  return true;
+}
 
 
 //===思路2===
