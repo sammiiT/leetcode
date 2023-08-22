0456aa34d01718086b7532319a93f48c71558f3e
sammiiT <dirsing@gmail.com>
2023-08-23 00:20:53 +0800

    Update 1091. Shortest Path in Binary Matrix.cpp

--- BFS/1091. Shortest Path in Binary Matrix.cpp
+++ BFS/1091. Shortest Path in Binary Matrix.cpp
@@ -8,6 +8,14 @@
 (*)一個node衍伸出node所有的child, 所有同一層的child, 都在同一層level運算
 -當計算每層child時, 都會累加一次
 - 如果同一層的child的座標發生了　ｘ== (n-1) && y==(n-1) , 則直接回傳累加次數, 此累加次數即為最少路徑 
+
+- 每一層用dirs來判斷; vector<int> dirs = {-1,1}; //每一層有兩個元素
+-vector<vector<int>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};//每一層有四個元素
+
+每一個element所衍生的所有"子元素", 是父元素的下一層
+for(int i=q.size();i>0;--i){//每一個element所衍生的所有"子元素", 是父元素的下一層
+vector<vector<int>> dirs = {{-1,0},{-1,1},{0,1},
+                                {1,1},{1,0},{1,-1},{0,-1},{-1,-1}};//每一層元素有8個
 //=====
 int shortestPathBinaryMatrix(vector<vector<int>>& grid){
     if(grid[0][0]==1) return -1;
