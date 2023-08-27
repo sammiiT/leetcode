3f120e377e4a736820147691eef9c93e99099053
sammiiT <dirsing@gmail.com>
2023-08-27 13:38:32 +0800

    Update 127. Word Ladder.cpp

--- BFS/127. Word Ladder.cpp
+++ BFS/127. Word Ladder.cpp
@@ -10,6 +10,27 @@ unordered_set<string> wordSet(wordList.begin(),wordList.end());
 wordSet中的key是 wordList中的每一個 string
 
 2.建立一個unordered_map<string,int> , 紀錄每一次convert之後的累加次數
+- 用unordered_map<string,int> 是紀錄每一個ladder出現的"歷史step數"
+範例:
+vector<string> wordList = {"hot","dot","dog","lot","log","cog"};
+歷史step_1:
+hit->hot->dot->dog->cog
+           | 
+           +--- dot的歷史step數為3 => 以BFS, dot會先接到dog, queue中會先計算到dog,cog
+                不會受到 "歷史step 2"和"歷史step_3"的影響, 即使dot被修改了, "歷史step_1"也不會受影響, 因為已經算到"dog"
+歷史step_2:        
+hit->hot->lot->dot->dog->cog
+           |     \
+           |      +--- dot的歷史step數為4
+           +-- lot的歷史step數為3
+        
+歷史step_3:
+hit->hot->dot->lot->log->cog
+           |    |
+           |    +--- lot的歷史step數4 
+           +--- dot的歷史step數為3 
+        
+        
 3.建立一個 queue<string> ,紀錄有出現在wordList中的string.
 
 (*)BFS的邏輯出現在:
