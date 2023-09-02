b38be0511de3e20bfe83fc43c8103bc420b9d4c4
sammiiT <dirsing@gmail.com>
2023-09-02 15:36:26 +0800

    Update 103 Binary Tree Zigzag Level Order Traversal.cpp

--- Tree/103 Binary Tree Zigzag Level Order Traversal.cpp
+++ Tree/103 Binary Tree Zigzag Level Order Traversal.cpp
@@ -32,3 +32,26 @@ public:
         helper(root->right,level+1,res);
     }
 };
+//===思路2===
+(*) BFS 解法
+
+vector<vector<int>> zigzagLevelOrder(TreeNode* root){
+    vector<vector<int>> res;
+    if(!root) return res;
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        res.push_back({});
+        for(int i=q.size(); i>0; --i){
+            TreeNode* p=q.front();q.pop();
+            if(res.size()%2) res.back().push_back(p->val);
+            else res.back().insert(res.back().begin(),p->val);
+            
+            if(p->left) q.push(p->left);
+            if(p->right) q.push(p->right);
+        }
+    }
+    return res;
+}
+
