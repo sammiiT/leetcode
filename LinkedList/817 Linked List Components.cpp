f2a19e49fd2a3d9a93543d781ed4dcb3d9f3849b
sammiiT <dirsing@gmail.com>
2023-09-30 16:29:33 +0800

    Update 817 Linked List Components.cpp

--- LinkedList/817 Linked List Components.cpp
+++ LinkedList/817 Linked List Components.cpp
@@ -1,4 +1,46 @@
-﻿class Solution {
+//===類似題===
+2239. Find Closest Number to Zero
+999. Available Captures for Rook
+1594. Maximum Non Negative Product in a Matrix
+
+//===思路===
+(*) list component 可以視為一個set
+1. 用unordered_map<int,int>  紀錄nums中出現的直
+2. 用unordered_set<int> 紀錄連續出現的數值:   
+- 遍歷linked_list並與unordered_map<int,int>中出現的值做比較, 如果出現,則屬於component的一部分, 記錄到unordered_set<int>中
+- 如果list node數值沒有出現在unordered_map中, 表示unorered_set<int>中的數值可以形成一個component , 將component數加一 
+
+3. 回傳最後的component數
+//===
+int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_map<int,int> ump;
+        unordered_set<int> st;
+        int res = 0;
+        ListNode* cur;
+        
+        for(int i:nums){
+            ump[i]=1;        
+        }   
+
+        cur = head;
+        while(cur){
+            if(ump.count(cur->val)){
+                st.insert(cur->val);//出現數值, 記錄在set中
+            }else{//
+                if(st.size()){//沒有出現在map中, 則將component數目加一
+                   res+=1; 
+                }
+                st.clear();//將set做reset動作
+            }
+            cur = cur->next;//繼續遍歷linked-list
+        }
+	
+        if(st.size()) res+=1;//如果set中還有數值, 要再將component數目加1
+        return res;
+}
+
+//===寫法2===
+class Solution {
 public:
     int numComponents(ListNode* head, vector<int>& G) {
 						//兩個數列做比較
