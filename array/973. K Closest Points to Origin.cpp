fe75c13ca35a950d52ebd6668c333c4425f31ad1
sammiiT <dirsing@gmail.com>
2023-10-29 15:45:27 +0800

    Update 973. K Closest Points to Origin.cpp

--- array/973. K Closest Points to Origin.cpp
+++ array/973. K Closest Points to Origin.cpp
@@ -3,9 +3,15 @@
 692. Top K Frequent Words
 1779. Find Nearest Point That Has the Same X or Y Coordinate
 
-//===思路====
+//===思路寫法1====
+vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    sort(points.begin(),points.end(),[](vector<int>& a, vector<int>& b){
+        return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1];
+    });
+    vector<vector<int>> res(points.begin(),points.begin()+k);
+    return res;
+}
 
-//=====
 vector<vector<int>> helper0(vector<vector<int>>& points, int k){
     vector<vector<int>> res;
     sort(points.begin(),points.end(),
@@ -20,7 +26,8 @@ vector<vector<int>> helper0(vector<vector<int>>& points, int k){
 vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         return helper0(points,k);
 }
-//====
+//===寫法2 ===
+(*)用priority_queue
 struct cmp{
   bool operator()(vector<int>& x ,vector<int>& y){
       return x[0]*x[0]+x[1]*x[1] > y[0]*y[0]+y[1]*y[1];
