1b24587d0ffdbfc81386275ab999403af2b2b3ba
sammiiT <dirsing@gmail.com>
2023-11-12 16:59:49 +0800

    Update 300 Longest Increasing Subsequence.cpp

--- DynamicProgramming/300 Longest Increasing Subsequence.cpp
+++ DynamicProgramming/300 Longest Increasing Subsequence.cpp
@@ -1,4 +1,35 @@
-﻿class Solution {
+//===類似題===
+354. Russian Doll Envelopes
+673. Number of Longest Increasing Subsequence
+712. Minimum ASCII Delete Sum for Two Strings
+//===思路===
+(*) dynamic programming
+
+dp[i]  物理意義, index從(0~i) 共有幾個value是increase
+dp[i]=max(dp[i],dp[j]+1)    // (+1)物理意義: 第i個數值,要+1
+
+
+ex:
+    0   1   2   3   4
+    1,  2,  3,  4,  5
+
+i=0;
+dp[1]=max(dp[1],dp[0]+1) =  1+1 =2
+
+i=2;
+dp[2] .... dp[0],dp[1]
+
+dp[2]=dp[0]+1 =2 
+dp[2]=dp[1]+1 =2+1=3
+
+i=3;
+dp[3] .... dp[0],dp[1],dp[2] 
+dp[3] = dp[0]+1 
+dp[3] = dp[1]+1
+dp[3] = dp[2]+1 = 4
+
+//=====
+class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         if(nums.empty()) return 0;
