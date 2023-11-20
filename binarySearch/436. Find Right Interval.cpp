ab71f29de4ec56c4319c22defab44f132ce65bd1
sammiiT <dirsing@gmail.com>
2023-11-20 23:49:21 +0800

    Update 436. Find Right Interval.cpp

--- binarySearch/436. Find Right Interval.cpp
+++ binarySearch/436. Find Right Interval.cpp
@@ -3,6 +3,12 @@
 352. Data Stream as Disjoint Intervals
 
 //===思路===
+(*)條件
+1. (startj >= endi) && (startj == minimize)
+
+2. starti為 unique
+- 可以用starti為基準 將intervals做排列, ascending排列
+- (startj == minimize) 用lower bound概念求最小 且 大於 endi的數值        
 
 //===
 int lower_bound(vector<vector<int>>& mat,int target){
@@ -11,7 +17,7 @@ int lower_bound(vector<vector<int>>& mat,int target){
         while(l<r){
             m=l+(r-l)/2;
             if(mat[m][0]<target) l=m+1;
-            else r=m;
+            else r=m;//mat[m][0] >=target
         }
         return r;//index
 }
@@ -20,9 +26,10 @@ vector<int> findRightInterval(vector<vector<int>>& intervals) {
         vector<int> res;
         vector<vector<int>> tmp = intervals;
         unordered_map<int,int> mp;
-
+                                                                //startj is minimize 
         sort(tmp.begin(),tmp.end(), [](vector<int>& a,vector<int>& b){ return a[0]<b[0];});
 
+        //starti is unique
         for(int i=0;i<intervals.size();++i) {mp[intervals[i][0]]=i;}
 
         for(int i=0;i<intervals.size();++i){
