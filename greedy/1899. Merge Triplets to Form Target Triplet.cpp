dce054746fa6481d122c19a5fbef47035a138920
sammiiT <dirsing@gmail.com>
2023-12-15 16:48:36 +0800

    Update 1899. Merge Triplets to Form Target Triplet.cpp

--- greedy/1899. Merge Triplets to Form Target Triplet.cpp
+++ greedy/1899. Merge Triplets to Form Target Triplet.cpp
@@ -10,8 +10,6 @@
 -在比較的過程中, 如果元素[i]大於target[i], 則排除不比較
 
 (*)最後的array是否等於target; 如果等於,則回傳true
-
-
 //=====
 bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
     vector<int> res(3,0);
@@ -23,4 +21,25 @@ bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         }
     }    
     return (res==target);    
+}
+
+//====寫法2====
+(*)概念同寫法1
+
+bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+    int n = triplets.size();
+    vector<int> tmp(triplets[0].size(),0); 
+    
+    for(int i=0 ;i<n; ++i){
+        int p,q,r;
+        if((p=max(tmp[0],triplets[i][0]))>target[0]) continue;
+        if((q=max(tmp[1],triplets[i][1]))>target[1]) continue;
+        if((r=max(tmp[2],triplets[i][2]))>target[2]) continue;
+        
+        if(p==target[0]&&q==target[1]&&r==target[2]) return true;    
+        tmp[0]=p;
+        tmp[1]=q;
+        tmp[2]=r;
+    }
+    return false;
 }
