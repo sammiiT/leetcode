179e62413e7e186858de3429219860bd546d65c9
sammiiT <dirsing@gmail.com>
2023-12-30 14:34:18 +0800

    Update 316. Remove Duplicate Letters.cpp

--- greedy/316. Remove Duplicate Letters.cpp
+++ greedy/316. Remove Duplicate Letters.cpp
@@ -9,7 +9,6 @@
 2.2 不對原字串做變動, 只對原字串做判斷
     解為新字串, 從對原字串做判斷而來
     
-    
 //===
 string helper0(string s){
     int mp[256]={0};
@@ -36,3 +35,40 @@ string helper0(string s){
 string removeDuplicateLetters(string s) {
         return helper0(s);
 }
+
+//===寫法2===
+string removeDuplicateLetters(string s) {
+    unordered_map<char,int> ump;//<character, count> 原本string的character個數
+    unordered_map<char,int> res_ump;//<character, count> 新string的character個數
+    string res;
+    
+    for(char c:s) ump[c]++;
+    for(int i=0; i<s.size(); ++i){
+        ump[s[i]]--;    
+        if(res.size()==0){
+            res = res + s[i];
+            res_ump[s[i]]++;
+        } else {
+            /*1.如果新string的最後一個char大於當下的char
+              2.如果新string的最後一個char, 存在於之後遍歷的元素中
+              3.如果新string中, 沒有存在當下的char
+              - 直接將新的string.back()移除
+            */
+            while(res.size() && res.back()>=s[i] && ump[res.back()] && !res_ump.count(s[i])){
+                res_ump[res.back()]--;
+                if(res_ump[res.back()]==0) res_ump.erase(res.back());
+                res.pop_back();
+            }
+            /*  1.移除完之後, 再判斷一次,是否有跟新string的元素重複
+                2.因為上面不成立有可能是, 1,2不成立
+            */
+            if(!res_ump.count(s[i])){
+                res.push_back(s[i]);
+                res_ump[s[i]]++;
+            }
+        }
+    }
+    return res;
+}
+
+
