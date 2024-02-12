c451bf6248cb82e44ad3dfccae41b503fe5141db
sammiiT <dirsing@gmail.com>
2024-02-13 00:24:15 +0800

    Update readme.cpp

--- sort/readme.cpp
+++ sort/readme.cpp
@@ -12,6 +12,32 @@
    
 
 //====merge sort ====
+
+void merge(vector<int>& arr, int l, int m, int r){
+    vector<int> left(arr.begin()+l, arr.begin()+m+1);
+    vector<int> right(arr.begin()+m+1,arr.begin()+r+1);
+ 
+    int i = 0;
+    int j = 0;
+    int k = l;
+    
+    while(i<left.size() || j <right.size()){
+        long lval = LONG_MAX, rval = LONG_MAX;
+        if(i<left.size()) lval = left[i]; 
+        if(j<right.size()) rval = right[j];
+        
+        if(lval<=rval){
+            arr[k]=lval;
+            i++;
+        }
+        else{//lval>rval
+            arr[k]=rval;
+            j++;
+        }
+        k++;
+    }
+}
+
 void merge(vector<int>& arr, int l, int m, int r){
     //獨立的array, 才可以重新把arr的數值覆蓋而不會出錯
     vector<int> left(arr.begin()+l,arr.begin()+m+1);//vector最後要預留一個空間
@@ -64,6 +90,6 @@ void quickSort_ascending(vector<int>& arr, int l, int r){
     }
 }
 
-
+//====bubble sort====
 
 
