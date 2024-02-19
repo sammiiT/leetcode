646ede728cd2000b476ed4e57da4383c90d1283b
sammiiT <dirsing@gmail.com>
2024-02-19 13:19:48 +0800

    Update 1485. Clone Binary Tree With Random Pointer.cpp

--- Tree/1485. Clone Binary Tree With Random Pointer.cpp
+++ Tree/1485. Clone Binary Tree With Random Pointer.cpp
@@ -32,3 +32,52 @@ Node* cloneTree(Node* tree){
   }
   return node;  
 }
+
+
+//===思路2(check)===
+
+(1)從old_node找到對應的old_node_random
+(2)old_node_random再從map中找到對應的new_node
+
+//map<Node*,Node*>//old_node,new_node
+Node* helper(Node* node, 
+            map<Node*,Node*>& mp){
+    
+    if(!node) return NULL;
+    
+    Node* n = new Node(node->key);
+    mp[node] = n;
+    
+    if(node->left) n->left  = helper(node->left,mp);
+    if(node->right) n->right = helper(node->right,mp);
+
+    return n;                
+}
+
+Node* cloneTree(Node* tree){
+    Node* node = NULL;
+    map<Node*,Node*> mp;
+    
+    node = helper(tree,mp);
+    
+    for(audo it:mp){
+/*        //it.first  = old_node
+        //it.second = new_node
+        
+        //old_node_random
+        it.first->random 
+    
+        //old_node_random to new_node
+        mp[it.first->random]
+        
+        //old_node to new_node
+        mp[it.first]*/
+        
+        Node* new_node = mp[it.first];
+        Node* new_node_random = mp[it.first->random];
+        new_node->random = new_node_random;
+    }
+    return node;    
+}
+
+
