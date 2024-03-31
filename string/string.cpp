9d4ceea211806908c32a7816649a9bc6a55c9fc0
sammiiT <dirsing@gmail.com>
2024-03-31 11:38:03 +0800

    Update string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -12,3 +12,10 @@ str.insert(str.begin(),'A');//插入character
 str.insert(0,"hello");//插入一個string
 
 str.erase(str.begin()+(str.size()-1));//移除最後一個character
+
+/*******
+常用功能
+*******/
+stoi(string)  : string轉integer; "005" 前面的00會被刪除
+to_string(int) : integer轉string
+str.substr(start,len): 擷取sub-string, 從index start開始,取長度len
