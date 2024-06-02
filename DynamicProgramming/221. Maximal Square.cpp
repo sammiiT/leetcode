f2847afe73a3e4e7c64f3961e6e705065328c113
sammiiT <dirsing@gmail.com>
2024-06-02 23:25:49 +0800

    Update 221. Maximal Square.cpp

--- DynamicProgramming/221. Maximal Square.cpp
+++ DynamicProgramming/221. Maximal Square.cpp
@@ -31,3 +31,37 @@ int maximalSquare(vector<vector<char>>& matrix) {
     }
     return res*res;
 }
+
+//===思路2===
+int maximalSquare(vector<vector<char>>& matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+    vector<vector<int>> dp(m,vector<int>(n,0));
+    int res = 0;
+    
+    for(int i=0; i<m; ++i) {
+        if(matrix[i][0]=='1'){
+            dp[i][0] = 1;
+            res = 1;
+        }
+    }
+    for(int i=0; i<n; ++i) {
+        if(matrix[0][i]=='1'){
+            dp[0][i] = 1;
+            res = 1;
+        }
+    }
+
+    for(int i=1; i<m; ++i){
+        for(int j=1; j<n; ++j){
+            if(matrix[i][j]=='1'){
+                if(matrix[i-1][j]=='1' && matrix[i][j-1]=='1'){
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j],dp[i][j-1])) + 1;
+                }else{
+                    dp[i][j]=1;
+                }
+            }
+            res = max(res,dp[i][j]);
+        }
+    }
+    return res*res;     
+}
