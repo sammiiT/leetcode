6f91bb9b690d0963fc43e8dd80df6c4f28d8ed12
sammiiT <dirsing@gmail.com>
2024-06-15 12:35:45 +0800

    Update 1062. Longest Repeating Substring.cpp

--- DynamicProgramming/1062. Longest Repeating Substring.cpp
+++ DynamicProgramming/1062. Longest Repeating Substring.cpp
@@ -3,6 +3,18 @@
 (*)因為二維dp的兩個字串是相同的字串;所以
 - 只討論"右上"區間;  
   
+  A T C G A T C G A
+A 1 0 0 0 1 0 0 0 1 
+T   x 0 0 0 2 0 0 0 
+C     x 0 0 0 3 0 0 
+G       x 0 0 0 4 0 
+A         x 0 0 0 5
+T           x 0 0 0 
+C             x 0 0 
+G               x 0 
+A                 x
+
+
 
 int longestRepeatingSubstring(string& S){
     int m = S.size();
