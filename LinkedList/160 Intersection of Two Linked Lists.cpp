4796bc45107dee0675fbf511ad020d02666375f3
sammiiT <dirsing@gmail.com>
2024-06-26 01:04:24 +0800

    Update 160 Intersection of Two Linked Lists.cpp

--- LinkedList/160 Intersection of Two Linked Lists.cpp
+++ LinkedList/160 Intersection of Two Linked Lists.cpp
@@ -4,6 +4,7 @@
 //===思路===
 (*)當作封閉list跑兩輪, 第一輪若沒有相等, 則第二輪從另外一個head開始
 (*)在兩輪之內一定可以找到答案, 不可能跑第三輪
+(*)將NULL節點,視為list運算的一部分
 //====
 ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
     ListNode* a,*b;
@@ -43,3 +44,51 @@ public:
         return a;
     }
 };
+
+//===寫法2===
+(*)不將NULL節點視為list的一部分; 運算時,遇到NULL點直接跳到頭節點
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *la,*lb;
+    ListNode hdrA,hdrB;
+
+    hdrA.next = headA;
+    hdrB.next = headB;
+
+    la = &hdrA;
+    lb = &hdrB;
+
+    while(la!=lb){
+        la = la->next;
+        lb = lb->next;
+        if(!la && !lb) break;
+
+        if(la==NULL) la = headB;
+        if(lb==NULL) lb = headA;
+    }
+    return la;
+}
+//===寫法3===
+(*)概念同寫法2, 不把NULL節點視為list的一部分.
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *la,*lb;
+    ListNode hdrA,hdrB;
+
+    hdrA.next = headA;
+    hdrB.next = headB;
+
+    la = &hdrA;
+    lb = &hdrB;
+
+    for(;;){
+        la = la->next;
+        lb = lb->next;
+        if(!la && !lb) break;
+        if(la==NULL) la=headB;
+        if(lb==NULL) lb=headA;
+        if(la==lb) return la;
+    }
+    return NULL;
+}
+
