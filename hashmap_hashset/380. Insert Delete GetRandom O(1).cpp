b57a27fd37ba80f76b85963798bbd79acc22700d
sammiiT <dirsing@gmail.com>
2024-07-06 12:03:48 +0800

    Update 380. Insert Delete GetRandom O(1).cpp

--- hashmap_hashset/380. Insert Delete GetRandom O(1).cpp
+++ hashmap_hashset/380. Insert Delete GetRandom O(1).cpp
@@ -2,7 +2,6 @@
 381. Insert Delete GetRandom O(1) - Duplicates allowed
 //===思路1====
 (*)利用multiset
-
 (*)題意:
 bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise
 所以要remove所有的重複item
@@ -80,4 +79,62 @@ private:
 map<int,int> ump;
 int total;
 };
+
+//=== 思路3 ====
+(*)本題是考 如何將vector<int> 在O(1)時間內做任意位置的"insert"和"remove"
+- 其中vector中的element是單一個
+(*)利用map<int,int>, 可達成上述目標
+map<int,int> mp;//<value,index> 紀錄 vector數值和對應的index
+每次insert就把值push到vector<int>的最後一位, 並將對應關係記錄到map中
+
+每次remove就把vector中的最後一個數值取出, 放置到被移除的element中;運算式如下
+        -------<
+      /          \ 
+  O  O  O  O      O(last)
+     \    
+       >remove(val)
+
+int last = nums.back();
+mp[last] = mp[val];//last數值現在要移到val述職的位置
+nums[mp[last]] = last;
+nums.pop_back();//將最後一個位置移除
+mp.erase(val);//將val從map中移除
+
+
+class RandomizedSet {
+public:
+    RandomizedSet() {
+    }
+    
+    bool insert(int val) {
+        bool ret = false;
+        if(mp.count(val)) return ret;
+        nums.push_back(val);
+        mp[val] = nums.size()-1;
+        return true;
+
+    }
+    
+    bool remove(int val) {
+        bool ret = false;
+        if(!mp.count(val)) return ret;
+
+        int lastn = nums.back();
+        mp[lastn] = mp[val];
+        nums[mp[lastn]] = lastn;
+        nums.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        int _rand = rand()%nums.size();
+        return nums[_rand];
+    }
+private:
+map<int,int> mp;//<value,count>
+vector<int> nums;
+};
+
+
 
