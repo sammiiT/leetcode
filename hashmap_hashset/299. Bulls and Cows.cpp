1d6330ab8de94b2846b9f24e52a53737f1543447
sammiiT <dirsing@gmail.com>
2024-07-06 23:31:58 +0800

    Update 299. Bulls and Cows.cpp

--- hashmap_hashset/299. Bulls and Cows.cpp
+++ hashmap_hashset/299. Bulls and Cows.cpp
@@ -39,3 +39,27 @@ string getHint(string secret, string guess) {
     res = to_string(A)+'A'+to_string(B)+'B';
     return res;
 }
+
+//===思路2===
+string getHint(string secret, string guess) {
+    unordered_map<char,int> scr;
+    unordered_map<char,int> gus;
+    int A=0,B=0;
+    
+    for(int i=0;i<secret.size();++i){
+        if(secret[i]==guess[i]){
+            A++;
+        }else{
+            scr[secret[i]]++;
+            gus[guess[i]]++;
+        }
+    }
+
+    for(auto it:gus){
+        char ch = it.first;
+        int count = it.second;
+        B = B+min(count,scr[ch]);
+    }
+
+   return to_string(A)+'A'+to_string(B)+'B';
+}
