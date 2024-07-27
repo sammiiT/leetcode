7836b348503f4b41298144775c423ad73d81d655
sammiiT <dirsing@gmail.com>
2024-07-27 12:41:03 +0800

    Update 1004. Max Consecutive Ones III.cpp

--- sliding window/1004. Max Consecutive Ones III.cpp
+++ sliding window/1004. Max Consecutive Ones III.cpp
@@ -78,4 +78,42 @@ int longestOnes(vector<int>& nums, int k) {
         return res;
 }
 
+//===思路4===
+(*) queue + sliding_window
+1.建立queue<int>,紀錄出現0的index, 較小的index會放在queue的前端
+- queue的size就是k:可以更改為"1"的"0"個數
+-遍歷數列, 如果出現0則 push到queue, 直到(queue.size==k)再做計算  
+-當(queue.size==0), 將queue做pop, 其內容為第一個"0"的index; 代表consecutive 1的長度從此index截斷, 要重新計算
 
+      0 1 2 3 4 5 6 7 8        0 1 2 3 4 5 6 7 8    
+nums  1,0,0,1,1,0,1,1,1    =>  1,0,0,1,1,0,1,1,1                  
+k=2      \ \                      \       \  
+          | q.push(2)              |       q.push(5) 
+        q.push(1)                  q.pop(): 從index1截斷, 長度從index2重新計算
+
+- 如果k=0時; 必須多一個q.empty()判斷;因為"空"做pop會有異常操作
+
+- 宣告變數i,j; i為consecutive_1的尾, j為consecutive_1的頭
+
+  
+int longestOnes(vector<int>& nums, int k){
+    queue<int> q;
+    int i=0,j=0,res=0;
+    for(i=0,j=0; i<nums.size(); ++i){
+        if(nums[i]==0){
+          int f = i;
+          if(q.size()<k){
+              q.push(i);
+          } else {//q.size()==k
+              if(!q.empty()){//如果k==0條件, 會發生此結果
+                  f = q.front();
+                  q.pop();
+                  q.push(i);
+              }
+              j = f+1;//重新紀錄conscutive_1的頭
+          }
+         }
+        res = max(res,i-j+1);    
+    }
+    return res;
+}
