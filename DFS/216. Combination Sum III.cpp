f6bc3d0ce648939d20cd141c5c9e4bb4d9749a12
sammiiT <dirsing@gmail.com>
2024-11-16 23:08:05 +0800

    Update 216. Combination Sum III.cpp

--- DFS/216. Combination Sum III.cpp
+++ DFS/216. Combination Sum III.cpp
@@ -50,4 +50,34 @@ void helper4(int k, int n, int start, vector<int>& out, vector<vector<int>>& res
 		out.pop_back();
 	}
 }
+//===寫法3===
+void dfs(int k, 
+        int n, 
+        int sum, 
+        int start, 
+        vector<int>& out, 
+        vector<vector<int>>& res){
+    if(sum==n && k==0){
+        res.push_back(out);
+        return;
+    }
+
+    for(int i=start;i<=9; ++i){
+        if((sum+i)>n) continue;
+        if((k-1)<0) continue;
+    
+        out.push_back(i);
+        dfs(k-1,n,sum+i,i+1,out,res);
+        out.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum3(int k, int n) {
+    vector<int> out;
+    vector<vector<int>> res;
+    dfs(k,n,0,1,out,res);
+    return res;         
+}
+
+
 
