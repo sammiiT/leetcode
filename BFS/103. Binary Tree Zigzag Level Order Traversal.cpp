7c27095fa767e6e7971b5fd9a9fc1254f4d6bef5
sammiiT <dirsing@gmail.com>
2025-02-22 11:46:20 +0800

    Update 103. Binary Tree Zigzag Level Order Traversal.cpp

--- BFS/103. Binary Tree Zigzag Level Order Traversal.cpp
+++ BFS/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -4,6 +4,7 @@
 1367. Linked List in Binary Tree
 //===思路===
 (*)BFS 算法
+push進queue中的順序都一樣, 只有在pop時, 依據level來判斷排序的先後順序
 //===
 vector<vector<int>> zigzagLevelOrder(TreeNode* root){
     vector<vector<int>> res;
@@ -23,4 +24,30 @@ vector<vector<int>> zigzagLevelOrder(TreeNode* root){
         }
     }
     return res;
+}
+
+===寫法2===
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    queue<TreeNode*> q;
+    vector<vector<int>> ret;
+    if(root!=NULL) q.push(root);
+    
+    int zigzag=0;
+    while(!q.empty()){
+        int size=q.size();
+        vector<int> nums;
+        
+        for(int i=size; i>0; --i){
+            TreeNode* p=q.front();q.pop();
+            if(zigzag%2==1) nums.insert(nums.begin(),p->val);
+            else nums.push_back(p->val);
+
+            if(p->left!=NULL) q.push(p->left);
+            if(p->right!=NULL) q.push(p->right);
+        }
+        zigzag++;
+        ret.push_back(nums);
+    }
+    return ret;
+
 }
