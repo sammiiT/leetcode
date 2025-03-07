7809c989e08d9989e36ee1386d7875f746096d0c
sammiiT <dirsing@gmail.com>
2025-03-07 17:01:40 +0800

    Update 377 Combination Sum IV.cpp

--- DynamicProgramming/377 Combination Sum IV.cpp
+++ DynamicProgramming/377 Combination Sum IV.cpp
@@ -84,25 +84,25 @@ public:
     }
 };
 
+#=== 寫法2====
+(*)dfs + mem
 
+int dfs(vector<int>& nums, int target, int sum, vector<int>& mem){
+    if(sum>target) return 0;
+    if(sum==target) return 1;
+    if(mem[target-sum]!=-1) return mem[target-sum];
 
-//     int combinationSum4(vector<int>& nums, int target) {
-//         vector<vector<int>> res;
-//         vector<int> out;
-//         sort(nums.begin(),nums.end());
-//         helper(nums, target, 0, out, res);
-        
-//         return res.size();
-//     }
-    
-    
-//     void helper(vector<int>& nums, int target, int start, vector<int> &out, vector<vector<int>>& res){
-//         if(target<0) return;
-//         if(target==0){ res.push_back(out); return; }
-        
-//         for(int i=start; i<nums.size(); ++i){
-//             out.push_back(nums[i]);
-//             helper(nums, target-nums[i], i, out, res);
-//             out.pop_back();
-//         }
-//     }
+    int count = 0;
+    for(int i=0;i<nums.size();++i){
+        int ret = dfs(nums,target,sum+nums[i],mem);
+        count+=ret;
+    }
+    mem[target-sum]=count;
+    return count;
+
+}
+
+int combinationSum4(vector<int>& nums, int target) {
+    vector<int> mem(target+1,-1);
+    return dfs(nums,target,0,mem);        
+}
